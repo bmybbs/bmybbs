128e082e6d6c73a83a1736b948285fc4eed7557c
IronBlood <aplshenyang@gmail.com>
2013-09-19 17:16:42 +0800

    新的通知列表样式

--- nju09/bbsnotify.c
+++ nju09/bbsnotify.c
@@ -13,20 +13,29 @@ int bbsnotify_main() {
 	check_msg();
 	changemode(READING);
 
+	// style 就直接写在这里了
+	printf("<style type=\"text/css\">\n"
+			"body { padding: 20px }\n"
+			"div#notify-content { width: 80% }\n"
+			"div#notify-content h1 { width: 100%; border-bottom: 1px #AAAAAA solid; padding-bottom: 12px }\n"
+			"div.notify-item { border-bottom: 1px #AAAAAA solid; padding: 12px 15px; }\n"
+			"div.notify-item a { display: inline-block; margin: 0 5px; }\n"
+			"</style>");
+
+	printf("<body><div id=\"notify-content\"><h1>我的提醒</h1>");
+
 	if(allNotifyItems == NULL) {
-		printf("<body>没有提醒</body>");
+		printf("<div class=\"notify-item\">没有提醒</div></div></body>");
 		http_quit();
 		return 0;
 	}
-
-	printf("<body><ul>");
 
 	struct NotifyItem * currItem;
 	for(currItem = (struct NotifyItem *)allNotifyItems; currItem != NULL; currItem = currItem->next) {
 		switch(currItem->type) {
 			case NOTIFY_TYPE_POST:
-				printf("<li>[<a href=\"bbsdelnotify?type=0&amp;B=%s&amp;ID=%d\">忽略</a>] "
-					   "网友<a href=\"qry?U=%s\">%s</a>%s在<a href=\"bbscon?B=%s&amp;F=M.%d.A\">%s</a>中回复了您</li>",
+				printf("<div class=\"notify-item\">[<a href=\"bbsdelnotify?type=0&amp;B=%s&amp;ID=%d\">忽略</a>] "
+					   "网友<a href=\"qry?U=%s\">%s</a>%s在<a href=\"bbscon?B=%s&amp;F=M.%d.A\">%s</a>中回复了您</div>",
 					    currItem->board, (int)currItem->noti_time,
 					    currItem->from_userid, currItem->from_userid, Difftime(currItem->noti_time),
 						currItem->board, (int)currItem->noti_time, currItem->title_gbk);
@@ -35,10 +44,8 @@ int bbsnotify_main() {
 		}
 	}
 	free_notification(allNotifyItems);
-
-	printf("</ul>");
 	printf("<div><a href=\"bbsdelnotify?type=%d\">忽略全部提醒</a></div>", NOTIFY_TYPE_NONSPECIFIED);
-	printf("</body>");
+	printf("</div></body>");
 	http_quit();
 	return 0;
 }
