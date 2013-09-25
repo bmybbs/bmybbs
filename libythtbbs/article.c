3d6a6b567342e65a2ed497ffe43007c3f2840c65
IronBlood <aplshenyang@gmail.com>
2013-09-25 12:50:04 +0800

    更新热门话题的时候使用互斥锁

--- libythtbbs/article.c
+++ libythtbbs/article.c
@@ -528,14 +528,20 @@ static int is_article_link_in_file(char *boardname, int thread, char *filename) 
  * @return 若文件已更新，则返回1
  */
 static int update_article_link_in_file(char *boardname, int oldthread, int newfiletime, char *newtitle, char *filename) {
+	int fd=open(filename, O_RDONLY);
+	if(fd == -1)
+		return -1;
+	flock(fd, LOCK_EX);  // 加锁
 	htmlDocPtr doc = htmlParseFile(filename, "GBK");
 	if(doc == NULL) {
+		flock(fd, LOCK_UN);
 		return -1;
 	}
 
 	xmlNodePtr pRoot = xmlDocGetRootElement(doc);
 	if(pRoot == NULL) {
 		xmlFreeDoc(doc);
+		flock(fd, LOCK_UN);
 		return -1;
 	}
 
@@ -564,7 +570,7 @@ static int update_article_link_in_file(char *boardname, int oldthread, int newfi
 	xmlXPathFreeObject(result);
 	xmlXPathFreeContext(ctx);
 	xmlFreeDoc(doc);
-
+	flock(fd, LOCK_UN);
 	return r;
 }
 
