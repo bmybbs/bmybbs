0eabc6ba7c6287024c9cec287e065ea8fcdc44ab
IronBlood <aplshenyang@gmail.com>
2013-10-09 00:40:02 +0800

    十大链接添加 title 属性

--- local_utl/newtop10.c
+++ local_utl/newtop10.c
@@ -312,8 +312,8 @@ html_topten(int mode, char *file)
 	for (j = 0; j < 10 && bt->unum != 0; j++, bt++) {
 		fprintf
 		    (fp,
-		     "<tr><td>第 %d 名</td><td><a href='tdoc?board=%s'>%s</a></td><td><div class='td-overflow'><a href='tfind?board=%s&amp;th=%d'>%s</a></div></td><td>%d</td></tr>\n",
-		     j + 1, bt->board, bt->board, bt->board, bt->thread, void1(nohtml(bt->title)),bt->unum);
+		     "<tr><td>第 %d 名</td><td><a href='tdoc?board=%s'>%s</a></td><td><div class='td-overflow'><a href='tfind?board=%s&amp;th=%d' title='%s'>%s</a></div></td><td>%d</td></tr>\n",
+		     j + 1, bt->board, bt->board, bt->board, bt->thread, void1(nohtml(bt->title)), void1(nohtml(bt->title)),bt->unum);
 	}
 	fprintf(fp, "</table></center></body>");
 	fclose(fp);
@@ -336,8 +336,8 @@ html_topten(int mode, char *file)
 		fprintf(fp, "%s<table width='90%'>", TDSTYLE);
 		for (j = 0; j < AREA_TOP_CNT && bt->unum != 0; j++, bt++) 
 		{
-			fprintf(fp, "<tr><td width='120px'>[<a href='tdoc?board=%s'>%s</a>]</td><td><div class='td-overflow'><a href='tfind?board=%s&amp;th=%d'>%s</a></div></td><td width='20px'>(%d)</td></tr>",
-				bt->board, bt->board, bt->board, bt->thread, void1(nohtml(bt->title)),bt->unum);
+			fprintf(fp, "<tr><td width='120px'>[<a href='tdoc?board=%s'>%s</a>]</td><td><div class='td-overflow'><a href='tfind?board=%s&amp;th=%d' title='%s'>%s</a></div></td><td width='20px'>(%d)</td></tr>",
+				bt->board, bt->board, bt->board, bt->thread, void1(nohtml(bt->title)), void1(nohtml(bt->title)),bt->unum);
 		}
 		fprintf(fp, "</table>");
 		fclose(fp);
@@ -365,8 +365,8 @@ index_topten(int mode, char *file)
         for (j = 0; j < 10 && bt->unum != 0; j++, bt++) {
                 fprintf
                     (fp,
-		     "<tr><td><span class=\"smalltext\">%d</span></td><td><div class='td-overflow'><a href='tfind?board=%s&th=%d'>%s</a></div></td></tr>\n",
-                     j + 1, bt->board, bt->thread, void1(nohtml(bt->title)));
+		     "<tr><td><span class=\"smalltext\">%d</span></td><td><div class='td-overflow'><a href='tfind?board=%s&th=%d' title='%s'>%s</a></div></td></tr>\n",
+                     j + 1, bt->board, bt->thread, void1(nohtml(bt->title)), void1(nohtml(bt->title)));
         }
         fclose(fp);
         rename("wwwtmp/indextopten.tmp", file);
