11d2546f941f0c37127ef280c3c392a0db30c341
IronBlood <aplshenyang@gmail.com>
2013-12-21 22:31:36 +0800

    使用颜色以及文件路径

--- bmy_grep.sh
+++ bmy_grep.sh
@@ -2,20 +2,15 @@
 # 对bmy代码进行grep
 # author: interma@bmy
 
-cd ./src
-grep -n $1 *.c
-grep -n $1 *.h
-cd ../nju09
-grep -n $1 *.c
-grep -n $1 *.h
-cd ../local_utl
-grep -n $1 *.c
-grep -n $1 *.h
-cd ../ythtlib
-grep -n $1 *.c
-grep -n $1 *.h
-cd ../libythtbbs
-grep -n $1 *.c
-grep -n $1 *.h
-cd ../include
-grep -n $1 *.h
+grep --color=auto -n $1 include/*.h
+grep --color=auto -n $1 ythtlib/*.c
+grep --color=auto -n $1 ythtlib/*.h
+grep --color=auto -n $1 libythtbbs/*.c
+grep --color=auto -n $1 libythtbbs/*.h
+grep --color=auto -n $1 src/*.c
+grep --color=auto -n $1 src/*.h
+grep --color=auto -n $1 nju09/*.c
+grep --color=auto -n $1 nju09/*.h
+grep --color=auto -n $1 local_utl/*.c
+grep --color=auto -n $1 local_utl/*.h
+
