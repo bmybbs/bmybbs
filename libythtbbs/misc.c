008869f2b4022446eaec120be3ad942b8d1094b6
IronBlood <aplshenyang@gmail.com>
2015-01-13 23:47:34 +0800

    Memory leak fixed by wrong iconv usage

--- libythtbbs/misc.c
+++ libythtbbs/misc.c
@@ -95,16 +95,18 @@ char *s;
 int code_convert(char *from_charset,char *to_charset,char *inbuf,size_t inlen,char *outbuf,size_t outlen)
 {
 	iconv_t cd;
-	int rc;
+	size_t rc;
 	char **pin = &inbuf;
 	char **pout = &outbuf;
 
 	cd = iconv_open(to_charset,from_charset);
 	if (cd==0) return -1;
 	memset(outbuf,0,outlen);
-	if (iconv(cd,pin,&inlen,pout,&outlen)==-1) return -1;
+
+	rc = iconv(cd,pin,&inlen,pout,&outlen);
 	iconv_close(cd);
-	return 0;
+
+	return (rc == (size_t) -1) ? -1 : 0;
 }
 
 //UNICODEÂë×ªÎªGB2312Âë
