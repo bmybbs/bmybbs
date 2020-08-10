7243bca806513c86cf6a29affa1b79e4386f6ede
IronBlood <aplshenyang@gmail.com>
2020-08-10 20:09:22 +0800

    移除 five.c 的编译链接

--- include/struct.h
+++ include/struct.h
@@ -3,21 +3,12 @@
     Copyright (C) 1990, Edward Luke, lush@Athena.EE.MsState.EDU
     Eagles Bulletin Board System
     Copyright (C) 1992, Raymond Rocker, rocker@rock.b11.ingr.com
-                         G    struct
-    {
-      char author[IDLEN + 1];
-      char board[18];
-      char title[66];
-      time_t date;
-      int number;
-    }      postlog;
-
-uy Vega, gtvega@seabass.st.usm.edu
+                        Guy Vega, gtvega@seabass.st.usm.edu
                         Dominic Tynes, dbtynes@seabass.st.usm.edu
     Firebird Bulletin Board System
     Copyright (C) 1996, Hsien-Tsung Chang, Smallpig.bbs@bbs.cs.ccu.edu.tw
                         Peng Piaw Foong, ppfoong@csie.ncu.edu.tw
-    
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 1, or (at your option)
@@ -28,6 +19,9 @@ uy Vega, gtvega@seabass.st.usm.edu
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 */
+
+#ifndef __BBS_STRUCT_H
+#define __BBS_STRUCT_H
 
 struct wwwsession {
 	unsigned char used:1, show_reg:1, att_mode:1, ipmask:4, doc_mode:1;
@@ -41,15 +35,15 @@ struct wwwsession {
 
 //modify by mintbaggio 040326 for front page commend
 struct commend{
-        char board[24];         //the board that the article be commened is in
-        char userid[14];        //the author of the article be commended
-        char com_user[14];      //the user who commend this article
-        char title[80];         //the title of the article be commended
-        char filename[80];      //the filename of the article be commended
-        //int time;
-        time_t time;            //the time when the com_user commend this article
-        //int flag;
-        unsigned int accessed;
+	char board[24];         //the board that the article be commened is in
+	char userid[14];        //the author of the article be commended
+	char com_user[14];      //the user who commend this article
+	char title[80];         //the title of the article be commended
+	char filename[80];      //the filename of the article be commended
+	//int time;
+	time_t time;            //the time when the com_user commend this article
+	//int flag;
+	unsigned int accessed;
 };
 
 #define TOKENLENGTH 8
@@ -90,7 +84,7 @@ struct user_info {		/* Structure used in UTMP file */
 	unsigned reject[MAXREJECTS];
 	struct wwwsession wwwinfo;
 	struct onebrc brc;
-	char user_state_temp[16];  //add by leoncom 
+	char user_state_temp[16];  //add by leoncom
 };
 
 #define BM_LEN 60
@@ -131,14 +125,14 @@ struct one_key {		/* Used to pass commands to the readmenu */
 };
 
 struct moneyCenter {
-        int ave_score;
-        int prize777;
-        int prize367;
-        int prizeSoccer;
-        unsigned char transfer_rate;
-        unsigned char deposit_rate;
-        unsigned char lend_rate;
-        unsigned char isSalaryTime:1,isSoccerSelling:1,isMCclosed:1,unused:5;
+	int ave_score;
+	int prize777;
+	int prize367;
+	int prizeSoccer;
+	unsigned char transfer_rate;
+	unsigned char deposit_rate;
+	unsigned char lend_rate;
+	unsigned char isSalaryTime:1,isSoccerSelling:1,isMCclosed:1,unused:5;
 };
 
 #define USHM_SIZE       (MAXACTIVE + 10)
@@ -207,9 +201,15 @@ struct postlog {
 	int number;
 };
 
-struct fivechess {
-	int winner;
-	int hand, tdeadf, tlivef, livethree, threefour;
-	int playboard[15][15];
-};
+// unused struct
+/*
+struct {
+	char author[IDLEN + 1];
+	char board[18];
+	char title[66];
+	time_t date;
+	int number;
+} postlog;
+*/
+#endif
 
