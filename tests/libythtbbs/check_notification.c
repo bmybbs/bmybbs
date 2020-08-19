f9569cecfa08aaa7db85feba2f0832a25d3a1f49
IronBlood <aplshenyang@gmail.com>
2020-08-19 20:37:01 +0800

    从 check_notification.c 中移除单元测试入口

--- tests/libythtbbs/check_notification.c
+++ tests/libythtbbs/check_notification.c
@@ -43,17 +43,3 @@ Suite * test_suite_notification(void) {
 
 	return s;
 }
-
-int main (void)
-{
-	int number_failed;
-
-	Suite *s = test_suite_notification();
-	SRunner *sr = srunner_create(s);
-	srunner_run_all(sr, CK_NORMAL);
-
-	number_failed = srunner_ntests_failed(sr);
-
-	srunner_free(sr);
-	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
