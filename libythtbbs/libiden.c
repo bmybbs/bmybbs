9d2b702619fb006d3d4a18d5c599f6ae4d5e3d8e
IronBlood <aplshenyang@gmail.com>
2020-08-22 11:18:30 +0800

    从配置文件中读取 mysql 连接信息（local_utl / nju09 仍然依赖存放连接信息的宏）

--- libythtbbs/libiden.c
+++ libythtbbs/libiden.c
@@ -40,7 +40,7 @@ char* str_to_uppercase(char *str)
     }
     return h;
 }
-
+
 char* str_to_lowercase(char *str)
 {
     char *h = str;
@@ -236,10 +236,40 @@ int query_record_num(char* value, int style)
     return count;
 }
 
+/**
+ * 从配置文件读取 sql 的连接信息，该函数属于 mysql_real_connect 的封装。
+ * 当配置文件存在且连接正常时，返回 MYSQL* 指针。
+ * @param s
+ * @return
+ */
 MYSQL * my_connect_mysql(MYSQL *s)
 {
+	const char *MYSQL_CONFIG_FILE = MY_BBS_HOME "/etc/mysqlconfig";
 
-    return mysql_real_connect(s, "localhost", SQLUSER, SQLPASSWD, SQLDB, 3306, NULL, 0);
+	FILE *cfg_fp;
+	int   cfg_fd;
+	char *sql_user[16];
+	char *sql_pass[16];
+	char *sql_db[24];
+	char *sql_port[4];
+	char *sql_host[32];
+
+	cfg_fp = fopen(MYSQL_CONFIG_FILE, "r");
+	if (!cfg_fp)
+		return NULL;
+
+	cfg_fd = fileno(cfg_fp);
+	flock(cfg_fd, LOCK_SH);
+
+	readstrvalue_fp(cfg_fp, "SQL_USER", sql_user, sizeof(sql_user));
+	readstrvalue_fp(cfg_fp, "SQL_PASS", sql_pass, sizeof(sql_pass));
+	readstrvalue_fp(cfg_fp, "SQL_DB", sql_db, sizeof(sql_db));
+	readstrvalue_fp(cfg_fp, "SQL_PORT", sql_port, sizeof(sql_port));
+	readstrvalue_fp(cfg_fp, "SQL_HOST", sql_host, sizeof(sql_host));
+
+	flock(cfg_fd, LOCK_UN);
+	fclose(cfg_fp);
+    return mysql_real_connect(s, sql_host, sql_user, sql_pass, sql_db, atoi(sql_port), NULL, CLIENT_IGNORE_SIGPIPE);
 }
 
 
