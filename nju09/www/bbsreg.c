d87f40927e9c62ac50028941f691e192d0c0d06f
IronBlood <aplshenyang@gmail.com>
2020-08-26 17:12:37 +0800

    nju09 注册界面移除邮箱密码

--- nju09/www/bbsreg.c
+++ nju09/www/bbsreg.c
@@ -6,11 +6,8 @@ bbsreg_main()
 {
 	html_header(1);
 
-#ifdef POP_CHECK
-	struct stat temp;
-#endif
-
 	printf("<body>");
+	printf("<style>code { background: #eee }</style>");
 	printf("<nobr><center>%s -- 新用户注册<hr>\n", BBSNAME);
 	printf("<font color=green>欢迎加入本站. 以下资料请如实填写. 带*号的项目为必填内容.</font>");
 	printf("<br><font color=green>其中 『 ID 』为您在本站所实际显示的用户名称，不可修改，请慎重填写.</font>");
@@ -30,25 +27,15 @@ bbsreg_main()
 	printf("<textarea name=words rows=3 cols=40 wrap=virutal></textarea>");
 
 #ifdef POP_CHECK
-	char bufpop[256];
-	int numpop = 0;
-	char namepop[10][256]; // 注意：最多信任10个pop服务器，要不就溢出了！
-	char ippop[10][256];
-	printf("<tr><td align=right>*可以信任的邮件服务器列表（新生请选择stu.xjtu.edu.cn）:<td align=left><SELECT NAME=popserver>\n");
+
+	printf("<tr><td align=right>*可以信任的邮件服务器列表:<td align=left><SELECT NAME=popserver>\n");
 	int n = 1;
 	while(n <= DOMAIN_COUNT)
 	{
-//		namepop[n - 1][strlen(namepop[n - 1]) - 1] = 0;
-//		ippop[n - 1][strlen(ippop[n - 1]) - 1] = 0;
-
-		char tempbuf[512];
-		strncpy(tempbuf, MAIL_DOMAINS[n], 256);
-		strcat(tempbuf, "+");
-		strcat(tempbuf, IP_POP[n]);
 		if (n == 1)
-			printf("<OPTION VALUE=%s SELECTED>", tempbuf);
+			printf("<OPTION VALUE=%s SELECTED>", MAIL_DOMAINS[n]);
 		else
-			printf("<OPTION VALUE=%s>", tempbuf);
+			printf("<OPTION VALUE=%s>", MAIL_DOMAINS[n]);
 
 		printf("%s", MAIL_DOMAINS[n]);
 		n++;
@@ -56,8 +43,7 @@ bbsreg_main()
 	}
 	printf("</select>\n");
 	printf("<tr><td align=right>*请输入邮箱用户名（新生请输入test）:<td align=left><input name=user size=20 maxlength=20> \n");
-	printf("<tr><td align=right>*请输入邮箱密码（新生请输入test）:<td align=left><input type=password name=pass size=20 maxlength=20> \n");
-	printf("<tr><td align=right>每个信箱最多可以认证 %d 个bbs帐号\n", MAX_USER_PER_RECORD);
+	printf("<tr><td align=right><td align=left>每个信箱最多可以认证 %d 个bbs帐号，此处仅需输入 @ 字符以前的部分，例如您的邮箱为 \"example@xjtu.edu.cn\"，此处输入 \"example\" 即可（不包含引号）。<br>邮箱域名请从列表中选择。", MAX_USER_PER_RECORD);
 
 #endif
 
