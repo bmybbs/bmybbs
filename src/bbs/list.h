f95254f76879de0fa43ae6a13f783e654a48db9f
IronBlood <aplshenyang@gmail.com>
2020-09-28 15:46:59 +0800

    新增 list 接口

---
+++ src/bbs/list.h
@@ -0,0 +1,14 @@
+#ifndef BMYBBS_LIST_H
+#define BMYBBS_LIST_H
+#include "struct.h"
+
+int myfriend(unsigned uid);
+int hisfriend(struct user_info *uentp);
+int isreject(struct user_info *uentp);
+int allusers(void);
+void setlistrange(int i);
+void Users(void);
+int t_friends(void);
+int t_users(void);
+int choose(int update, int defaultn, void (*title_show)(void), int (*key_deal)(int, int, int), int (*list_show)(void), int (*read)(int, int));
+#endif //BMYBBS_LIST_H
