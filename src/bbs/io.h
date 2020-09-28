175330b4babf01da99f69f44046fc59ce5f64b35
IronBlood <aplshenyang@gmail.com>
2020-09-28 16:09:39 +0800

    新增 io 接口

--- src/bbs/io.h
+++ src/bbs/io.h
@@ -1,4 +1,14 @@
 #ifndef BMYBBS_IO_H
 #define BMYBBS_IO_H
+void oflush(void);
+void init_alarm(void);
+void ochar(int c);
+void io_output(const char *s, int len);
+void add_io(int fd, int timeout);
+void add_flush(void (*flushfunc)(void));
+int num_in_buf(void);
+int igetkey(void);
+int ask(char *prompt);
 int getdata(int line, int col, char *prompt, char *buf, int len, int echo, int clearlabel);
+int multi_getdata(int line, int col, int maxcol, char *prompt, char *buf, int len, int maxline, int clearlabel);
 #endif //BMYBBS_IO_H
