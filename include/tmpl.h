f937bcb4cf8ed60c6dceedcc060f9bb8dff05f62
IronBlood <aplshenyang@gmail.com>
2020-09-29 13:44:25 +0800

    将 tmpl 结构体移动回头文件

--- include/tmpl.h
+++ include/tmpl.h
@@ -1,5 +1,28 @@
 #ifndef _TEMPLATE_H
 #define _TEMPLATE_H
+#include "config.h"
+
+struct s_content {
+	char text[50];
+	size_t length;
+};
+
+struct s_template {
+	char title[50];
+	char authorid[IDLEN];
+	char title_prefix[20];
+	int content_num;
+	char filename[STRLEN];
+	int flag;
+	int version;
+	char unused[16];
+	char title_tmpl[STRLEN];
+};
+
+struct a_template {
+	struct s_template * tmpl;
+	struct s_content * cont;
+};
 
 int m_template(void);
 int choose_tmpl(void);
--- src/bbs/tmpl.c
+++ src/bbs/tmpl.c
@@ -16,28 +16,6 @@
 #define MAX_CONTENT_LENGTH 555
 #define TMPL_NOW_VERSION 1
 
-struct s_content {
-	char text[50];
-	size_t length;
-};
-
-struct s_template {
-	char title[50];
-	char authorid[IDLEN];
-	char title_prefix[20];
-	int content_num;
-	char filename[STRLEN];
-	int flag;
-	int version;
-	char unused[16];
-	char title_tmpl[STRLEN];
-};
-
-struct a_template {
-	struct s_template * tmpl;
-	struct s_content * cont;
-};
-
 extern int page, range;
 extern char IScurrBM;
 extern struct boardmem *bcache;
