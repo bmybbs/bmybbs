8eb7ad7d984246401bb67824be622cd8093377c5
IronBlood <aplshenyang@gmail.com>
2020-10-07 23:22:45 +0800

    format code

--- src/bbs/bbsgopher.c
+++ src/bbs/bbsgopher.c
@@ -10,7 +10,7 @@
                         Peng Piaw Foong, ppfoong@csie.ncu.edu.tw
 
     Copyright (C) 1999, KCN,Zhou Lin, kcn@cic.tsinghua.edu.cn
-    
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 1, or (at your option)
