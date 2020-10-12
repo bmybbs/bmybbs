fdcd6cb3fde203e6495ae7c0b1d526fa25c063ad
IronBlood <aplshenyang@gmail.com>
2020-10-13 01:04:43 +0800

    bbsfriend.c 使用 ythtbbs_cache_UserTable_get_user_online_friends 重构

--- nju09/www/bbsfriend.c
+++ nju09/www/bbsfriend.c
@@ -4,38 +4,28 @@ int
 bbsfriend_main()
 {
 	int i, total = 0;
-	struct user_info user[MAXFRIENDS];
+	struct user_info user_list[MAXFRIENDS];
 	struct user_info *x;
 	html_header(1);
 	check_msg();
 	changemode(FRIEND);
 	printf("<body><center>\n");
 	printf("<div class=rhead>%s -- 在线好友列表 [使用者:<span class=h11> %s</span>]</div><hr>\n", BBSNAME, currentuser.userid);
-	for (i = 0; i < MAXACTIVE && total < MAXFRIENDS; i++) {
-		x = &(shm_utmp->uinfo[i]);
-		if (x->active == 0)
-			continue;
-		if (x->invisible && !HAS_PERM(PERM_SEECLOAK, currentuser))
-			continue;
-		if (!isfriend(x->userid))
-			continue;
-		memcpy(&user[total], x, sizeof (struct user_info));
-		total++;
-	}
+	total = ythtbbs_cache_UserTable_get_user_online_friends(currentuser.userid, HAS_PERM(PERM_SEECLOAK, currentuser), user_list, MAXFRIENDS);
+	qsort(user_list, total, sizeof (struct user_info), (void *) cmpuser);
 	printf("<table border=1>\n");
 	printf("<tr><td>序号</td><td>友</td><td>使用者代号</td><td>使用者昵称</td><td>来自</td><td>动态</td><td>发呆</td></tr>\n");
-	qsort(user, total, sizeof (struct user_info), (void *) cmpuser);
 	for (i = 0; i < total; i++) {
-		int dt = (now_t - user[i].lasttime) / 60;
+		int dt = (now_t - user_list[i].lasttime) / 60;
 		printf("<tr><td>%d</td>", i + 1);
 		printf("<td>%s</td>", "√");
-		printf("<td><a href=bbsqry?userid=%s>%s</a></td>", user[i].userid, user[i].userid);
-		printf("<td><a href=bbsqry?userid=%s>%24.24s</a></td>", user[i].userid, nohtml(void1(user[i].username)));
-		printf("<td><font class=c%d>%20.20s</font></td>", user[i].pid == 1 ? 35 : 32, user[i].from);
-		if(user[i].mode != USERDF4)
-		printf("<td>%s</td>", user[i].invisible ? "隐身中..." : ModeType(user[i].mode));
+		printf("<td><a href=bbsqry?userid=%s>%s</a></td>", user_list[i].userid, user_list[i].userid);
+		printf("<td><a href=bbsqry?userid=%s>%24.24s</a></td>", user_list[i].userid, nohtml(void1(user_list[i].username)));
+		printf("<td><font class=c%d>%20.20s</font></td>", user_list[i].pid == 1 ? 35 : 32, user_list[i].from);
+		if(user_list[i].mode != USERDF4)
+		printf("<td>%s</td>", user_list[i].invisible ? "隐身中..." : ModeType(user_list[i].mode));
 		else
-		printf("<td>%s</td>", user[i].invisible ? "隐身中..." : user[i].user_state_temp);
+		printf("<td>%s</td>", user_list[i].invisible ? "隐身中..." : user_list[i].user_state_temp);
 		if (dt == 0) {
 			printf("<td></td></tr>\n");
 		} else {
