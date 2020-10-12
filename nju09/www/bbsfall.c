e9b54bd68d3e589a07481056deb2c10e86d02aad
IronBlood <aplshenyang@gmail.com>
2020-10-12 14:28:51 +0800

    format code

--- nju09/www/bbsfall.c
+++ nju09/www/bbsfall.c
@@ -11,22 +11,18 @@ bbsfall_main()
 	changemode(GMENU);
 	loadfriend(currentuser.userid);
 	printf("<body><center>\n");
-	printf("<div class=rhead>%s -- 好友名单 [使用者: <span class=h11>%s</span>]</div><hr><br>\n", BBSNAME,
-	       currentuser.userid);
+	printf("<div class=rhead>%s -- 好友名单 [使用者: <span class=h11>%s</span>]</div><hr><br>\n", BBSNAME, currentuser.userid);
 	printf("您共设定了 %ld 位好友<br>", friendnum);
-	printf
-	    ("<table border=1><tr><td>序号</td><td>好友代号</td><td>好友说明</td><td>删除好友</td></tr>");
+	printf("<table border=1><tr><td>序号</td><td>好友代号</td><td>好友说明</td><td>删除好友</td></tr>");
 	for (i = 0; i < friendnum; i++) {
 		printf("<tr><td>%d</td>", i + 1);
-		printf("<td><a href=bbsqry?userid=%s>%s</a></td>", fff[i].id,
-		       fff[i].id);
+		printf("<td><a href=bbsqry?userid=%s>%s</a></td>", fff[i].id, fff[i].id);
 		printf("<td>%s</td>\n", nohtml(fff[i].exp));
-		printf
-		    ("<td>[<a onclick='return confirm(\"确实删除吗?\")' href=bbsfdel?userid=%s>删除</a>]</td></tr>",
-		     fff[i].id);
+		printf("<td>[<a onclick='return confirm(\"确实删除吗?\")' href=bbsfdel?userid=%s>删除</a>]</td></tr>", fff[i].id);
 	}
 	printf("</table><hr>\n");
 	printf("[<a href=bbsfadd>添加新的好友</a>]</center></body>\n");
 	http_quit();
 	return 0;
 }
+
