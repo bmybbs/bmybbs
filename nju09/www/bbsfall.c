0e5ae0b9943fac593556cf43662f5b9690a1d6b6
IronBlood <aplshenyang@gmail.com>
2020-10-12 15:08:04 +0800

    bbsfall 使用 ythtbbs_override_ 接口重构

--- nju09/www/bbsfall.c
+++ nju09/www/bbsfall.c
@@ -4,12 +4,18 @@ int
 bbsfall_main()
 {	//modify by mintbaggio 20040829 for new www
 	unsigned int i;
+	int lockfd;
 	html_header(1);
 	check_msg();
 	if (!loginok || isguest)
 		http_fatal("您尚未登录, 请先登录");
 	changemode(GMENU);
-	loadfriend(currentuser.userid);
+
+	lockfd = ythtbbs_override_lock(currentuser.userid, YTHTBBS_OVERRIDE_FRIENDS);
+	friendnum = ythtbbs_override_count(currentuser.userid, YTHTBBS_OVERRIDE_FRIENDS);
+	ythtbbs_override_get_records(currentuser.userid, fff, friendnum, YTHTBBS_OVERRIDE_FRIENDS);
+	ythtbbs_override_unlock(lockfd);
+
 	printf("<body><center>\n");
 	printf("<div class=rhead>%s -- 好友名单 [使用者: <span class=h11>%s</span>]</div><hr><br>\n", BBSNAME, currentuser.userid);
 	printf("您共设定了 %ld 位好友<br>", friendnum);
