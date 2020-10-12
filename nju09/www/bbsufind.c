a427b132ff9ef2f07c5fd84915208c8771ad3ba3
IronBlood <aplshenyang@gmail.com>
2020-10-13 01:17:44 +0800

    format code

--- nju09/www/bbsufind.c
+++ nju09/www/bbsufind.c
@@ -1,18 +1,18 @@
 #include "bbslib.h"
 
-void            
+void
 printkick(char *buf, int size, struct user_info *x)
-{               
-        if (!isguest && x->pid == 1
-            &&          
-            ((currentuser.userlevel & PERM_SYSOP
-              || !strcmp(currentuser.userid, x->userid))))
-                    snprintf(buf, size, 
-                             "<a onclick='return confirm(\"你真的要踢出该用户吗?\")' href=kick?k=%s&f=%s>X</a>",
-                             x->userid, x->from);
-        else
-                buf[0] = 0;
-}       
+{
+	if (isguest || x->pid != 1) {
+		buf[0] = 0;
+		return;
+	}
+
+	if((currentuser.userlevel & PERM_SYSOP) || !strcmp(currentuser.userid, x->userid))
+		snprintf(buf, size, "<a onclick='return confirm(\"你真的要踢出该用户吗?\")' href=kick?k=%s&f=%s>X</a>", x->userid, x->from);
+	else
+		buf[0] = 0;
+}
 
 int
 bbsufind_main()
@@ -26,8 +26,7 @@ bbsufind_main()
 	check_msg();
 	changemode(LUSERS);
 	printf("<body><center>\n");
-	printf("<div class=rhead>%s -- 在线用户查询 [在线总人数:<span class=h11> %d</span>人]</div><hr>\n", BBSNAME,
-	       count_online());
+	printf("<div class=rhead>%s -- 在线用户查询 [在线总人数:<span class=h11> %d</span>人]</div><hr>\n", BBSNAME, count_online());
 	search = toupper(getparm("search")[0]);
 	limit = atoi(getparm("limit"));
 	if (limit <=0)
@@ -63,28 +62,20 @@ bbsufind_main()
 	}
 
 	printf("<table border=1>\n");
-	printf
-	    ("<tr><td>序号</td><td>友</td><td>使用者代号</td><td>使用者昵称</td><td>来自</td><td>动态</td><td>发呆</td></tr>\n");
+	printf("<tr><td>序号</td><td>友</td><td>使用者代号</td><td>使用者昵称</td><td>来自</td><td>动态</td><td>发呆</td></tr>\n");
 	qsort(user, total, sizeof (struct user_info), (void *) cmpuser);
 	for (i = 0; i < total; i++) {
 		int dt = (now_t - user[i].lasttime) / 60;
 		printf("<tr><td>%d</td>", i + 1);
 		printf("<td>%s</td>", isfriend(user[i].userid) ? "√" : "  ");
 		printkick(buf, sizeof (buf), &user[i]);
-		printf("<td><a href=bbsqry?userid=%s>%s</a> %s</td>",
-		       user[i].userid, user[i].userid, buf);
-		printf("<td><a href=bbsqry?userid=%s>%24.24s</a></td>",
-		       user[i].userid, nohtml(void1(user[i].username)));
-		printf("<td><font class=c%d>%20.20s</font></td>",
-		       user[i].pid == 1 ? 35 : 32, user[i].from);
+		printf("<td><a href=bbsqry?userid=%s>%s</a> %s</td>", user[i].userid, user[i].userid, buf);
+		printf("<td><a href=bbsqry?userid=%s>%24.24s</a></td>", user[i].userid, nohtml(void1(user[i].username)));
+		printf("<td><font class=c%d>%20.20s</font></td>", user[i].pid == 1 ? 35 : 32, user[i].from);
 		if(user[i].mode != USERDF4 )
-		printf("<td>%s</td>",
-		       user[i].
-		       invisible ? "隐身中..." : ModeType(user[i].mode));
+			printf("<td>%s</td>", user[i].invisible ? "隐身中..." : ModeType(user[i].mode));
 		else
-		printf("<td>%s</td>",
-		       user[i].
-		       invisible ? "隐身中..." : user[i].user_state_temp);
+			printf("<td>%s</td>", user[i].invisible ? "隐身中..." : user[i].user_state_temp);
 		if (dt == 0) {
 			printf("<td></td></tr>\n");
 		} else {
@@ -106,8 +97,7 @@ bbsufind_main()
 //		}
 	}
 	printf("<br>\n");
-	printf
-	    ("[<a href='javascript:history.go(-1)'>返回</a>]");
+	printf("[<a href='javascript:history.go(-1)'>返回</a>]");
 	printf("</center></body>\n");
 	http_quit();
 	return 0;
