156ced41b758c192ed59e28ea7a5b210022f5246
IronBlood <aplshenyang@gmail.com>
2020-10-14 14:09:12 +0800

    使用 ythtbbs_user_ 和 bmy_cookie_ 接口重写

--- nju09/www/bbslogin.c
+++ nju09/www/bbslogin.c
@@ -1,256 +1,73 @@
 #include "bbslib.h"
 #include "ytht/random.h"
+#include "bmy/cookie.h"
 
-#define NSEARCH MAXACTIVE
-static char *makeurlbase(int uent);
-static const char *makeguesturl(void);
-static char *check_multi(char *id, int uid);
-static char *wwwlogin(struct userec *user);
+int bbslogin_main() {
+	int rc;
+	char buf[128], id[20], pw[20];
+	struct user_info info;
+	struct bmy_cookie cookie;
 
-int
-bbslogin_main()
-{
-	int n, t;
-	time_t dtime;
-	char filename[128], buf[256], id[20], pw[20], *ub = FIRST_PAGE; // main_page[STRLEN];
-	struct userec *x;
-	html_header(3);
 	ytht_strsncpy(id, getparm("id"), 13);
 	ytht_strsncpy(pw, getparm("pw"), 13);
 
 	if (loginok && strcasecmp(id, currentuser.userid) && !isguest) {
 		http_fatal("系统检测到目前你的计算机上已经登录有一个帐号 %s，请先退出.(选择正常logout)", currentuser.userid);
 	}
+
 	if (!strcmp(id, "")) {
 		strcpy(id, "guest");
 	}
-	x = getuser(id);
-	if (x == 0) {
-		printf("%s<br>", id);
-		http_fatal("错误的使用者帐号");
-	}
-	strcpy(id, x->userid);
-	if (strcasecmp(id, "guest")) {
-		if (checkbansite(fromhost)) {
-			http_fatal("对不起, 本站不欢迎来自 [%s] 的登录. <br>若有疑问, 请与SYSOP联系.", fromhost);
-		}
-		if (userbansite(x->userid, fromhost))
-			http_fatal("本ID已设置禁止从%s登录", fromhost);
-		if (!ytht_crypt_checkpasswd(x->passwd, pw)) {
-			logattempt(x->userid, fromhost, "WWW", now_t);
+
+	rc = ythtbbs_user_login(id, pw, fromhost, YTHTBBS_LOGIN_NJU09, &info, &currentuser, NULL);
+	if (rc != YTHTBBS_USER_LOGIN_OK) {
+		switch(rc) {
+		case YTHTBBS_USER_NOT_EXIST:
+			http_fatal("错误的使用者帐号");
+			break;
+		case YTHTBBS_USER_WRONG_PASSWORD:
 			http_fatal("密码错误");
-		}
-		if (!user_perm(x, PERM_BASIC))
+			break;
+		case YTHTBBS_USER_SUSPENDED:
 			http_fatal("此帐号已被停机, 若有疑问, 请用其他帐号在sysop版询问.");
-		if (file_has_word(MY_BBS_HOME "/etc/prisonor", x->userid))
-			http_fatal("安心改造，不要胡闹");
-		t = x->lastlogin;
-		x->lastlogin = now_t;
-		if (abs(t - now_t) < 20) {
+			break;
+		case YTHTBBS_USER_SITE_BAN:
+			http_fatal("对不起, 本站不欢迎来自 [%s] 的登录. <br>若有疑问, 请与SYSOP联系.", fromhost);
+			break;
+		case YTHTBBS_USER_USER_BAN:
+			http_fatal("本ID已设置禁止从%s登录", fromhost);
+			break;
+		case YTHTBBS_USER_TOO_FREQUENT:
 			http_fatal("两次登录间隔过密!");
+			break;
+		case YTHTBBS_USER_IN_PRISON:
+			http_fatal("安心改造，不要胡闹");
+			break;
+		case YTHTBBS_USER_ONLINE_FULL:
+			http_fatal("抱歉，目前在线用户数已达上限，无法登录。请稍后再来。");
+			break;
 		}
+	}
 
-		dtime = t - 4 * 3600;
-		t = localtime(&dtime)->tm_mday;
-		dtime = now_t - 4 * 3600;
-		if (t < localtime(&dtime)->tm_mday && x->numdays < 800)
-			x->numdays++;
-		x->numlogins++;
-		ytht_strsncpy(x->lasthost, fromhost, BMY_IPV6_LEN);
-		save_user_data(x);
-		currentuser = *x;
-	}
-	sprintf(buf, "%s enter %s www", x->userid, fromhost);
-	newtrace(buf);
-	n = 0;
-	if (loginok && isguest) {
-		bzero(u_info, sizeof (struct user_info));
-	}
 	if (strcasecmp(id, "guest")) {
-		sethomepath(filename, x->userid);
-		mkdir(filename, 0755);
-
-		ytht_strsncpy(buf, getparm("style"), 3);
-		wwwstylenum = -1;
-		if (isdigit(buf[0]))
+		if (!readuservalue(info.userid, "wwwstyle", buf, sizeof (buf)))
 			wwwstylenum = atoi(buf);
-		if ((wwwstylenum > NWWWSTYLE || wwwstylenum < 0))
-			if (!readuservalue(x->userid, "wwwstyle", buf, sizeof (buf)))
-				wwwstylenum = atoi(buf);
 		if (wwwstylenum < 0 || wwwstylenum >= NWWWSTYLE)
 			wwwstylenum = 1;
-		currstyle = &wwwstyle[wwwstylenum];
 	} else {
 		wwwstylenum = 1;
-		currstyle = &wwwstyle[wwwstylenum];
 	}
 
-	ub = wwwlogin(x);
-	redirect(ub);
+	cookie.userid = info.userid;
+	cookie.sessid = info.sessionid;
+	cookie.token  = info.token;
+	cookie.extraparam = getextrparam_str(wwwstylenum);
+	bmy_cookie_gen(buf, sizeof(buf), &cookie);
+
+	printf("Set-Cookie: " SMAGIC "=%s; SameSite=Strict; HttpOnly;", buf);
+	html_header(3);
+	redirect("/" SMAGIC "/"); // URL 不再附带 session 信息
 	http_quit();
 	return 0;
-}
-
-static char *wwwlogin(struct userec *user) {
-	FILE *fp, *fp1;
-	int n, dolog = 0, st, clubnum, uid, i, nsearch;
-	struct user_info *u;
-	char ULIST[STRLEN];
-	char genbuf[256], *urlbase, fname[80];
-	uid = getusernum(user->userid) + 1;
-
-	if ((urlbase = check_multi(user->userid, uid)))
-		return urlbase;
-
-	if (strcasecmp(user->userid, "guest") && count_uindex(uid) >= 3)
-		http_fatal("您已经登录了三个帐号,不能再登录了");
-//      如果要限制WWW登录窗口数 就打开这个注释. lepton
-
-	gethostname(genbuf, 256);
-	sprintf(ULIST, MY_BBS_HOME "/%s.%s", ULIST_BASE, genbuf);
-
-	fp = fopen(ULIST, "a");
-	flock(fileno(fp), LOCK_EX);
-	nsearch = NSEARCH;
-	//if (strcasecmp(user->userid, "guest"))
-	//      nsearch = MAXACTIVE / 4;
-	for (i = 0, n = 0; i < nsearch; i++, n++) {
-		if (n >= MAXACTIVE)
-			n = 0;
-		u = &(shm_utmp->uinfo[n]);
-		if (u->active && u->pid == 1 && ((now_t - u->lasttime) > 20 * 60 || u->wwwinfo.iskicked)) {
-			st = u->lasttime - u->wwwinfo.login_start_time;
-			if (st > 86400) {
-				errlog("Strange long stay time,%d!, drop %s", st, u->userid);
-				st = 86400;
-			}
-			sprintf(genbuf, "%s drop %d www", u->userid, st);
-			newtrace(genbuf);
-			remove_uindex(u->uid, n + 1);
-			bzero(u, sizeof (struct user_info));
-		}
-		if (!dolog && u->active == 0) {
-			u_info = u;
-			bzero(u, sizeof (struct user_info));
-			u->active = 1;
-			u->uid = uid;
-			u->pid = 1;
-			//u->pid = thispid;			//modify by mintbaggio@BMY for kill www user
-			u->mode = LOGIN;
-			if (strcasecmp(user->userid, "guest"))
-				u_info->unreadmsg = get_unreadmsg(user->userid);
-			else
-				u_info->unreadmsg = 0;
-			u->userlevel = user->userlevel;
-			u->lasttime = now_t;
-			u->curboard = 0;
-			if (user_perm(user, PERM_LOGINCLOAK) && (user->flags[0] & CLOAK_FLAG))
-				u->invisible = YEA;
-			u->pager = 0;
-			if (user->userdefine & DEF_FRIENDCALL)
-				u->pager |= FRIEND_PAGER;
-			if (user->flags[0] & PAGER_FLAG) {
-				u->pager |= ALL_PAGER;
-				u->pager |= FRIEND_PAGER;
-			}
-			if (user->userdefine & DEF_FRIENDMSG)
-				u->pager |= FRIENDMSG_PAGER;
-			if (user->userdefine & DEF_ALLMSG) {
-				u->pager |= ALLMSG_PAGER;
-				u->pager |= FRIENDMSG_PAGER;
-			}
-			ytht_strsncpy(u->from, fromhost, BMY_IPV6_LEN);
-			ytht_strsncpy(u->username, user->username, NAMELEN);
-			ytht_strsncpy(u->userid, user->userid, IDLEN + 1);
-			ytht_get_random_str(u->sessionid);
-			if (strcasecmp(user->userid, "guest")) {
-				//initfriends(u); // use ythtbbs_user_init_override instead
-				urlbase = makeurlbase(n);
-			} else {
-				memset(u->friend, 0, sizeof(u->friend));
-				urlbase = makeguesturl();
-			}
-
-			w_info = &(u_info->wwwinfo);
-			w_info->login_start_time = now_t;
-			if (strcasecmp(user->userid, "guest")) {
-				sethomefile(fname, user->userid, "clubrights");
-				if ((fp1 = fopen(fname, "r")) == NULL) {
-					memset(u_info->clubrights, 0, 4 * sizeof (int));
-				} else {
-					while (fgets(genbuf, STRLEN, fp1) != NULL) {
-						clubnum = atoi(genbuf);
-						u_info->clubrights[clubnum / 32] |= (1 << clubnum % 32);
-					}
-					fclose(fp1);
-				}
-
-				set_my_cookie();
-			} else {
-				memset(u_info->clubrights, 0, 4 * sizeof (int));
-				w_info->t_lines = 20;
-				w_info->att_mode = 0;
-				w_info->doc_mode = 1;
-			}
-			dolog = 1;
-			add_uindex(u->uid, n + 1);
-		}
-	}
-	flock(fileno(fp), LOCK_UN);
-	fclose(fp);
-	if (!dolog)
-		http_fatal("抱歉，目前在线用户数已达上限，无法登录。请稍后再来。");
-	if ((user->userlevel & PERM_BOARDS))
-		setbmstatus(user, 1);
-	return urlbase;
-}
-
-static int
-do_check(int uent, int uid)
-{
-	return (shm_utmp->uinfo[uent].active == 1
-		&& shm_utmp->uinfo[uent].pid == 1
-		&& now_t - shm_utmp->uinfo[uent].lasttime < 18 * 60
-		&& shm_utmp->uinfo[uent].uid == uid
-		&& !strncmp(fromhost, shm_utmp->uinfo[uent].from, BMY_IPV6_LEN));
-}
-
-static char *
-makeurlbase(int uent)
-{
-	static char urlbase[STRLEN];
-	//max 26*26*26-1 = 17575 online,enough?
-	sprintf(urlbase, "/%s%c%c%c%s", SMAGIC,
-		uent / (26 * 26) + 'A', uent / 26 % 26 + 'A',
-		uent % 26 + 'A', shm_utmp->uinfo[uent].sessionid);
-	addextraparam(urlbase, sizeof (urlbase), 0, wwwstylenum);
-	strcat(urlbase, "/");
-	return urlbase;
-}
-
-static const char *
-makeguesturl(void) {
-	return "/" SMAGIC "_B/";
-}
-
-static char *
-check_multi(char *id, int uid)
-{
-	int i, uent;
-	if (uid <= 0 || uid > MAXUSERS)
-		return NULL;
-	if (strcasecmp(id, "guest") != 0) {
-		//这种算法, wwwlogin必须限制登录窗口数目, 否则
-		//上线名单会被轻易冲爆
-		for (i = 3; i < 6; i++) {
-			uent = uindexshm->user[uid - 1][i] - 1;
-			if (do_check(uent, uid))
-				return makeurlbase(uent);
-		}
-		return NULL;
-	} else {
-		return makeguesturl();
-	}
-	return NULL;
 }
 
