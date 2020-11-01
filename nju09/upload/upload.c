ef03de78309d10db2d971e08bdd91eb1dde583f3
IronBlood <aplshenyang@gmail.com>
2020-11-02 01:30:09 +0800

    format code

--- nju09/upload/upload.c
+++ nju09/upload/upload.c
@@ -68,13 +68,11 @@ getpathsize(char *path, int showlist)
 			break;
 		}
 		sprintf(fname, "%s/%s", path, pdent->d_name);
-		size = file_size(fname);
+		size = ytht_file_size_s(fname);
 		printf("<ul>\n");
 		if (showlist) {
-			printf("<li> <b>%s</b> (<i>%d字节</i>) ",
-			       pdent->d_name, size);
-			printf("<a href='%s&%s'>删除</a></li>\n",
-			       getreqstr(), pdent->d_name);
+			printf("<li> <b>%s</b> (<i>%d字节</i>) ", pdent->d_name, size);
+			printf("<a href='%s&%s'>删除</a></li>\n", getreqstr(), pdent->d_name);
 		}
 		printf("</ul>\n");
 		if (size < 0) {
@@ -85,8 +83,7 @@ getpathsize(char *path, int showlist)
 	}
 	closedir(pdir);
 	if (showlist) {
-		printf("<br>大小总计 %d 字节 (最大 %d 字节)<br>", totalsize,
-		       MAXATTACHSIZE);
+		printf("<br>大小总计 %d 字节 (最大 %d 字节)<br>", totalsize, MAXATTACHSIZE);
 	}
 	return totalsize;
 }
@@ -106,14 +103,13 @@ http_fatal(char *str)
 	exit(0);
 }
 
-/* Skip a line in the input stream. */
-static void
-SkipLine(char **Input,		/* Pointer into the incoming stream. */
-	 int *InputLength)
-{				/* Bytes left in the incoming stream. */
-
-	while ((**Input != '\0') && (**Input != '\r')
-	       && (**Input != '\n')) {
+/**
+ * Skip a line in the input stream.
+ * @param Input        Pointer into the incoming stream.
+ * @param InputLength  Bytes left in the incoming stream.
+ */
+static void SkipLine(char **Input, int *InputLength) {
+	while ((**Input != '\0') && (**Input != '\r') && (**Input != '\n')) {
 		*Input = *Input + 1;
 		*InputLength = *InputLength - 1;
 	}
@@ -127,28 +123,30 @@ SkipLine(char **Input,		/* Pointer into the incoming stream. */
 	}
 }
 
-static void
-GoAhead(char **Input,		/* Pointer into the incoming stream. */
-	int *InputLength, int len)
-{
+/**
+ * @brief TODO
+ * @param Input        Pointer into the incoming stream.
+ * @param InputLength  TODO
+ * @param len          TODO
+ */
+static void GoAhead(char **Input, int *InputLength, int len) {
 	*Input += len;
 	*InputLength -= len;
 }
 
-/* Accept a single segment from the incoming mime stream. Each field in the
-   form will generate a mime segment. Return a pointer to the beginning of
-   the Boundary, or NULL if the stream is exhausted. */
-static void
-AcceptSegment(char **Input,	/* Pointer into the incoming stream. */
-	      int *InputLength,	/* Bytes left in the incoming stream. */
-	      char *Boundary	/* Character string that delimits segments. */
-    )
-{
+/**
+ * Accept a single segment from the incoming mime stream. Each field in the
+ * form will generate a mime segment. Return a pointer to the beginning of
+ * the Boundary, or NULL if the stream is exhausted.
+ * @param Input        Pointer into the incoming stream.
+ * @param InputLength  Bytes left in the incoming stream.
+ * @param Boundary     Character string that delimits segments.
+ */
+static void AcceptSegment(char **Input, int *InputLength, char *Boundary) {
 	char *FieldName;	/* Name of the variable from the form. */
 	char *ContentEnd;
 	char *contentstr, *ptr;
-	/* The input stream should begin with a Boundary line. Error-exit if not
-	   found. */
+	/* The input stream should begin with a Boundary line. Error-exit if not found. */
 	if (strncmp(*Input, Boundary, strlen(Boundary)) != 0)
 		http_fatal("文件传送错误 10");
 	/* Skip the Boundary line. */
@@ -157,9 +155,9 @@ AcceptSegment(char **Input,	/* Pointer into the incoming stream. */
 	/* Return NULL if the stream is exhausted (no more segments). */
 	if ((**Input == '\0') || (strncmp(*Input, "--", 2) == 0))
 		http_fatal("文件传送错误 11");
-	/* The first line of a segment must be a "Content-Disposition" line. It
-	   contains the fieldname, and optionally the original filename. Error-exit
-	   if the line is not recognised. */
+	// The first line of a segment must be a "Content-Disposition" line. It
+	// contains the fieldname, and optionally the original filename. Error-exit
+	// if the line is not recognised.
 	contentstr = "content-disposition: form-data; name=\"";
 	if (strncasecmp(*Input, contentstr, strlen(contentstr)))
 		http_fatal("文件传送错误 12");
@@ -183,15 +181,15 @@ AcceptSegment(char **Input,	/* Pointer into the incoming stream. */
 	*ptr = 0;
 	ptr++;
 	GoAhead(Input, InputLength, ptr - *Input);
-	/* Skip the Disposition line and one or more mime lines, until an empty
-	   line is found. */
+	// Skip the Disposition line and one or more mime lines, until an empty
+	// line is found.
 	SkipLine(Input, InputLength);
 	while ((**Input != '\r') && (**Input != '\n'))
 		SkipLine(Input, InputLength);
 	SkipLine(Input, InputLength);
-	/* The following data in the stream is binary. The Boundary string is the
-	   end of the data. There may be a CRLF just before the Boundary, which
-	   must be stripped. */
+	// The following data in the stream is binary. The Boundary string is the
+	// end of the data. There may be a CRLF just before the Boundary, which
+	// must be stripped.
 	ContentStart = *Input;
 	ContentLength = 0;
 	while (*InputLength > 0 && memcmp(*Input, Boundary, strlen(Boundary))) {
@@ -225,7 +223,7 @@ save_attach()
 	char *pSuffix;//add by wsf
 	int suffixLen;//add by wsf
 	FILE *fp;
-	
+
 
 	p0 = FileName;
 	ptr = strrchr(p0, '/');
@@ -252,15 +250,13 @@ save_attach()
 			--pAscii;
 		pAscii = (pAscii+1) & 1;
 		strcpy(&p0[40-suffixLen-pAscii],pSuffix);
-		printf("<script language=\"JavaScript\">\n"
-                                        " alert('文件名超过40个字符长度，已经进行了截取');\n"
-                                        "</script>\n");
+		printf("<script language=\"JavaScript\">alert('文件名超过40个字符长度，已经进行了截取');</script>\n");
 	}
-	
+
 	if (checkfilename(p0)){
 		printf("<script language=\"JavaScript\">\n"
-                                        " alert(\"文件名中不能包含空格或者下面的非法字符\\r\\n\\/~`!@#$%%^&*()|{}[];:\\\"'<>,?\");\n"
-                                        "</script>\n");
+				" alert(\"文件名中不能包含空格或者下面的非法字符\\r\\n\\/~`!@#$%%^&*()|{}[];:\\\"'<>,?\");\n"
+				"</script>\n");
 		http_fatal("无效的文件名");
 	}
 	//修改结束 add by wsf
@@ -311,17 +307,17 @@ printuploadform()
 {
 	char *req = getreqstr();
 	printf("<hr>"
-	       "<form name=frmUpload action='%s' enctype='multipart/form-data' method=post>"
-	       "上载附件: <input type=file name=file>"
-	       "<input type=submit value=上载 "
-	       "onclick=\"this.value='附件上载中，请稍候...';this.disabled=true;frmUpload.submit();\">"
-	       "</form> "
-	       "在这里可以为文章附加点小图片小程序啥的, 不要贴太大的东西哦, 文件名里面也不要有括号问号什么的, 否则会粘贴失败哦.<br>"
-	       "<b>可以在文章中任意定位附件</b>，只需要在文章编辑框中预期位置上顶头写上“#attach 1.jpg”就可以了(别忘了将 1.jpg 换成所上载的文件名 :) )<br>"
-		   "还要注意，如果是图片的话，<b>单个图片文件最大大小为 %d byte</b>."
-	       "<center><input type=button value='刷新' onclick=\"location='%s';\">&nbsp; &nbsp;"
-	       "<input type=button value='完成' onclick='window.close();'></center>",
-	       req, MAXPICSIZE, req);
+			"<form name=frmUpload action='%s' enctype='multipart/form-data' method=post>"
+			"上载附件: <input type=file name=file>"
+			"<input type=submit value=上载 "
+			"onclick=\"this.value='附件上载中，请稍候...';this.disabled=true;frmUpload.submit();\">"
+			"</form> "
+			"在这里可以为文章附加点小图片小程序啥的, 不要贴太大的东西哦, 文件名里面也不要有括号问号什么的, 否则会粘贴失败哦.<br>"
+			"<b>可以在文章中任意定位附件</b>，只需要在文章编辑框中预期位置上顶头写上“#attach 1.jpg”就可以了(别忘了将 1.jpg 换成所上载的文件名 :) )<br>"
+			"还要注意，如果是图片的话，<b>单个图片文件最大大小为 %d byte</b>."
+			"<center><input type=button value='刷新' onclick=\"location='%s';\">&nbsp; &nbsp;"
+			"<input type=button value='完成' onclick='window.close();'></center>",
+			req, MAXPICSIZE, req);
 }
 
 int
@@ -342,9 +338,7 @@ main(int argc, char *argv[], char *environment[])
 
 	if (geteuid() != BBSUID)
 		http_fatal("内部错误 0");
-	shm_utmp =
-	    (struct UTMPFILE *) get_old_shm(UTMP_SHMKEY,
-					    sizeof (struct UTMPFILE));
+	shm_utmp = (struct UTMPFILE *) get_old_shm(UTMP_SHMKEY, sizeof (struct UTMPFILE));
 	if (!shm_utmp)
 		http_fatal("内部错误 1");
 
@@ -408,12 +402,11 @@ main(int argc, char *argv[], char *environment[])
 		//inet_aton(fromhost, &from_addr);
 	}
 	/*
-	if (!uin.active || strcmp(uin.sessionid, str + 4)
-	    || strncmp(uin.from, fromhost,20))
+	if (!uin.active || strcmp(uin.sessionid, str + 4) || strncmp(uin.from, fromhost,20))
 		http_fatal("请先登录 3");
 	*/
 
-	if (!uin.active) 
+	if (!uin.active)
 		http_fatal("请先登录 31");
 	if (strcmp(uin.sessionid, str + 4))
 		http_fatal("请先登录 31");
@@ -421,8 +414,7 @@ main(int argc, char *argv[], char *environment[])
 		http_fatal("请先登录 31");
 	if (!(uin.userlevel & PERM_POST))
 		http_fatal("缺乏 POST 权限");
-	snprintf(userattachpath, sizeof (userattachpath), PATHUSERATTACH "/%s",
-		 uin.userid);
+	snprintf(userattachpath, sizeof (userattachpath), PATHUSERATTACH "/%s", uin.userid);
 	mkdir(userattachpath, 0760);
 	//clearpath(userattachpath);
 	/* Test if the program was started by a METHOD=POST form. */
@@ -441,16 +433,16 @@ main(int argc, char *argv[], char *environment[])
 	ptr = getsenv("CONTENT_TYPE");
 	if (strncasecmp(ptr, "multipart/form-data; boundary=", 30))
 		http_fatal("文件传送错误 2");
-	/* Determine the Boundary, the string that separates the segments in the
-	   stream. The boundary is available from the CONTENT_TYPE environment
-	   variable. */
+	// Determine the Boundary, the string that separates the segments in the
+	// stream. The boundary is available from the CONTENT_TYPE environment
+	// variable.
 	ptr = strchr(ptr, '=');
 	if (!ptr)
 		http_fatal("文件传送错误 3");
 	ptr++;
 	ytht_strsncpy(Boundary + 2, ptr, sizeof(Boundary) - 2);
-	/* Get the total number of bytes in the input stream from the
-	   CONTENT_LENGTH environment variable. */
+	// Get the total number of bytes in the input stream from the
+	// CONTENT_LENGTH environment variable.
 	len = atoi(getsenv("CONTENT_LENGTH"));
 	if (len <= 0 || len > 5000000)
 		http_fatal("文件传送错误 4");
@@ -461,17 +453,17 @@ main(int argc, char *argv[], char *environment[])
 	buf[len] = 0;
 	ptr = buf;
 	AcceptSegment(&ptr, &len, Boundary);
-	
+
 	// 图片大小限制
 	ptr = FileName+strlen(FileName);
-	if (!strcasecmp(ptr - 4, ".gif") || !strcasecmp(ptr - 4, ".jpg") || 
-	    !strcasecmp(ptr - 4, ".bmp") || !strcasecmp(ptr - 4, ".png"))
-	    if (ContentLength > MAXPICSIZE)
-	    	{
-	    		free(buf);
-				http_fatal("图片附件太大, 超过限额");
-	    	}
-	
+	if (!strcasecmp(ptr - 4, ".gif") || !strcasecmp(ptr - 4, ".jpg") ||
+			!strcasecmp(ptr - 4, ".bmp") || !strcasecmp(ptr - 4, ".png")) {
+		if (ContentLength > MAXPICSIZE) {
+			free(buf);
+			http_fatal("图片附件太大, 超过限额");
+		}
+	}
+
 	if (ContentLength + attachtotalsize > MAXATTACHSIZE) {
 		free(buf);
 		http_fatal("附件太大, 超过限额");
@@ -484,3 +476,4 @@ main(int argc, char *argv[], char *environment[])
 		printuploadform();
 	return (0);
 }
+
