5d4fd378e29da6e5c7ddca7f6554dd20c012271f
IronBlood <aplshenyang@gmail.com>
2020-11-04 17:25:20 +0800

    format code

--- nju09/www/bbskick.c
+++ nju09/www/bbskick.c
@@ -1,20 +1,15 @@
 #include "bbslib.h"
-static void
-printkickitem(const struct user_info *x)		//modify by mintbaggio@BMY
-{
+
+static void printkickitem(const struct user_info *x) {
+	//modify by mintbaggio@BMY
 	int dt = (now_t - x->lasttime) / 60;
 	if (x->active == 0) {
 		return;
 	}
 	printf("<tr>");
 	printf("<td><a href=bbsqry?userid=%s>%s</a></td>", x->userid, x->userid);
-/*	printf
-	    ("<a onclick='return confirm(\"你真的要踢出该用户吗?\")' href=kick?k=%s&f=%s>X</a></td>",
-	     x->userid, x->from);*/
-	printf("<td><a href=bbsqry?userid=%s>%s</a></td>", x->userid,
-	       x->userid);
-	printf("<td><font class=c%d>%20.20s</font></td>", x->pid == 1 ? 35 : 32,
-	       x->from);
+	printf("<td><a href=bbsqry?userid=%s>%s</a></td>", x->userid, x->userid);
+	printf("<td><font class=c%d>%20.20s</font></td>", x->pid == 1 ? 35 : 32, x->from);
 	if(x -> mode != USERDF4 )
 	printf("<td>%s</td>", x->invisible ? "隐身中..." : ModeType(x->mode));
 	else
@@ -24,9 +19,7 @@ printkickitem(const struct user_info *x)		//modify by mintbaggio@BMY
 	} else {
 		printf("<td>%d</td>\n", dt);
 	}
-	printf
-            ("<td><a onclick='return confirm(\"你真的要踢出该用户吗?\")' href=kick?k=%s&f=%s>Kick</a></td></tr>",
-             x->userid, x->from);
+	printf("<td><a onclick='return confirm(\"你真的要踢出该用户吗?\")' href=kick?k=%s&f=%s>Kick</a></td></tr>", x->userid, x->from);
 }
 
 static void
@@ -35,8 +28,7 @@ printkicklist(const char *usertokick)
 	struct user_info *x;
 	int i, multilognum = 0;
 	printf("<hr><table border=1>\n");
-	printf
-	    ("<tr><td>序号</td><td>使用者代号</td><td>使用者昵称</td><td>动态</td><td>发呆</td><td>踢人</td></tr>\n");
+	printf("<tr><td>序号</td><td>使用者代号</td><td>使用者昵称</td><td>动态</td><td>发呆</td><td>踢人</td></tr>\n");
 	for (i = 0; i < MAXACTIVE; i++) {
 		x = &(shm_utmp->uinfo[i]);
 		if (x->active == 0 || x->pid != 1)
@@ -68,14 +60,12 @@ bbskick_main()
 		http_fatal("匆匆过客不能进行此操作, 请先登录");
 	changemode(LUSERS);
 	tokick = getparm("k");
-	if (strlen(tokick) == 0) {	//没有用户名参数k就要求输入一个,但不检查f
-		printf
-		    ("<hr><br><center><form action=kick name=userkicker method=post>");
-		printf
-		    ("请输入希望查询其在线情况的用户id  <input type=text name=k ><input type=submit value=查看></form><hr>");
+	if (strlen(tokick) == 0) {
+		//没有用户名参数k就要求输入一个,但不检查f
+		printf("<hr><br><center><form action=kick name=userkicker method=post>");
+		printf("请输入希望查询其在线情况的用户id  <input type=text name=k ><input type=submit value=查看></form><hr>");
 	} else {
-		if (!(currentuser.userlevel & PERM_SYSOP)
-		    && strcmp(currentuser.userid, tokick))
+		if (!(currentuser.userlevel & PERM_SYSOP) && strcmp(currentuser.userid, tokick))
 			http_fatal("错误的参数");
 		f = getparm("f");
 		// f长度为0时一般认为是从菜单访问的
@@ -86,9 +76,7 @@ bbskick_main()
 		} else {
 			for (i = 0; i < MAXACTIVE; i++) {
 				x = &(shm_utmp->uinfo[i]);
-				if (x->active == 0 ||
-				    x->pid != 1 || strcmp(x->userid, tokick) ||
-				    strcmp(x->from, f))
+				if (x->active == 0 || x->pid != 1 || strcmp(x->userid, tokick) || strcmp(x->from, f))
 					continue;
 				x->wwwinfo.iskicked = 1;
 				printf("已经踢出用户");
@@ -99,3 +87,4 @@ bbskick_main()
 	}
 	return 0;
 }
+
