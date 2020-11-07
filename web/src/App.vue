12a5d1b0ec04e3c30ec20d31352062918983799d
IronBlood <aplshenyang@gmail.com>
2020-11-08 06:06:30 +0800

    web 工程框架 (based on vue-3)

---
+++ web/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<Login />
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+
+export default {
+	name: "App",
+	components: {
+		Login
+	}
+};
+</script>
+
+<style>
+</style>
+
---
+++ web/src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+	name: "Login"
+};
+</script>
+
+<style scoped>
+</style>
+
