d9aaca517ee486d0c73b2c0bbaf2bd781641fc5c
IronBlood <aplshenyang@gmail.com>
2020-11-10 19:19:41 +0800

    登录页进站画面导航初步实现（使用 vue 的方式）

--- web/src/components/Login.vue
+++ web/src/components/Login.vue
@@ -1,12 +1,173 @@
 <template>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="bmy-content">
+				<ul class="index-nav float-right">
+					<li><a>匿名登录</a></li>
+					<li><a>telnet登录</a></li>
+					<li><a>CTerm工具下载</a></li>
+					<li><a>新用户注册</a></li>
+				</ul>
+				<div id="pic-container">
+					<div class="slides" v-for="item in loginpics" :key="item.img_url" v-bind:style="{ display: item.display ? 'block' : 'none' }">
+						<a v-bind:href="item.img_link"><img v-bind:src="item.img_url"></a>
+					</div>
+
+					<div class="prev" v-on:click="showPrev">&#10094;</div>
+					<div class="next" v-on:click="showNext">&#10095;</div>
+				</div>
+				<div id="pic-nav">
+					<div v-for="item in loginpics" :key="item.img_url" v-bind:class="{ active: item.display }"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-	name: "Login"
+	name: "Login",
+	data() {
+		return {
+			slideIndex: 1,
+			loginpics: [],
+			loginok: false,
+			checked: false
+		}
+	},
+	created() {
+		console.log("created");
+	},
+	mounted() {
+		console.log("mounted");
+		let that = this;
+		fetch("/BMY/loginpics")
+			.then(response => response.json())
+			.then(response => {
+				let str = response.data,
+					arr = str.split(";;");
+
+				arr.forEach((el, idx) => {
+					let tmp = el.split(";");
+					console.log(idx);
+					that.loginpics.push({
+						display: (idx == 0),
+						img_url: tmp[0],
+						img_link: tmp[1]
+					});
+				});
+			});
+	},
+	methods: {
+		showSlides(n) {
+			let i;
+
+			if (n > this.loginpics.length) {
+				this.slideIndex = 1;
+			}
+
+			if (n < 1) {
+				this.slideIndex = this.loginpics.length;
+			}
+
+			for (i = 0; i < this.loginpics.length; i++) {
+				this.loginpics[i].display = this.slideIndex == i + 1;
+			}
+		},
+
+		showPrev() {
+			this.showSlides(this.slideIndex += 1);
+		},
+
+		showNext() {
+			this.showSlides(this.slideIndex += 1);
+		}
+	}
 };
 </script>
 
 <style scoped>
+.container-fluid {
+	padding-top: 47px;
+}
+
+.bmy-content {
+	width: 794px;
+	margin: auto;
+}
+
+.bmy-content ul.index-nav {
+	list-style: none;
+	color: #aaa;
+	font-size: 12px;
+	margin-bottom: 5px;
+}
+
+.bmy-content ul.index-nav li {
+	display: inline;
+}
+
+.bmy-content ul.index-nav li:after {
+	content: "/";
+	padding-left: .3em;
+	padding-right: .3em;
+	color: #000;
+}
+
+.bmy-content ul.index-nav li:last-child:after {
+	content: "";
+	padding: none;
+}
+
+#pic-container {
+	display: relative;
+}
+
+.prev,
+.next {
+	cursor: pointer;
+	position: absolute;
+	top: 50%;
+	width: auto;
+	padding: 16px;
+	margin-top: -20px;
+	color: white;
+	font-weight: bold;
+	font-size: 20px;
+	border-radius: 0 3px 3px 0;
+	user-select: none;
+	background: rgba(0, 0, 0, 0.1);
+	-webkit-user-select: none;
+}
+
+.next {
+	right: 0;
+	border-radius: 3px 0 0 3px;
+}
+
+.prev:hover, .next:hover {
+	background-color: rgba(0, 0, 0, 0.8);
+}
+
+#pic-nav {
+	width: 100px;
+	margin: 0 auto;
+}
+
+#pic-nav div {
+	border: 1px solid #FF6600;
+	display: inline-block;
+	width: 12px;
+	height: 12px;
+	margin-right: 5px;
+}
+
+#pic-nav div:last-child {
+	margin-right: 0px;
+}
+
+#pic-nav div.active {
+	background-color: #FF6600;
+}
 </style>
 
