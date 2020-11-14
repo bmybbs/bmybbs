7d105a8002deacd2a35357855e93481934d3d95b
IronBlood <aplshenyang@gmail.com>
2020-11-14 18:18:17 +0800

    重构: nju09/www 使用 ythtbbs_cache_Board_foreach_v 接口重构精华区下载

--- nju09/www/bbsadl.c
+++ nju09/www/bbsadl.c
@@ -1,25 +1,28 @@
 #include "bbslib.h"
 
+static int bbsadl_callback(struct boardmem *board, int curr_idx, va_list ap) {
+	int *no = va_arg(ap, int *);
+	char buf[256];
+	snprintf(buf, sizeof(buf), "www/an/%s.tgz", board->header.filename);
+	if (!file_exist(buf))
+		return 0;
+	*no = *no + 1;
+	printf("<tr><td>%d<td><a href=/an/%s.tgz>%s.tgz</a><td>%ld<td>%s\n",
+			*no, board->header.filename, board->header.filename, file_size(buf), ytht_ctime(file_time(buf)) + 4);
+	return 0;
+}
+
 int
 bbsadl_main()
 {
-	int i, no = 0;
-	char brd[100], buf[256];
+	int no = 0;
 	html_header(1);
 	check_msg();
 	printf("<nobr><center>%s -- 精华区下载服务<hr>\n", BBSNAME);
 	printf("<table>\n");
 	printf("<tr><td>序号<td>名称<td>大小(字节)<td>更新时间\n");
-	for (i = 0; i < MAXBOARD && i < shm_bcache->number; i++) {
-		ytht_strsncpy(brd, shm_bcache->bcache[i].header.filename, 60);
-		sprintf(buf, "www/an/%s.tgz", brd);
-		if (!file_exist(buf))
-			continue;
-		no++;
-		printf
-		    ("<tr><td>%d<td><a href=/an/%s.tgz>%s.tgz</a><td>%ld<td>%s\n",
-			 no, brd, brd, file_size(buf), ytht_ctime(file_time(buf)) + 4);
-	}
+	ythtbbs_cache_Board_foreach_v(bbsadl_callback, no);
 	printf("</table>");
 	return 0;
 }
+
