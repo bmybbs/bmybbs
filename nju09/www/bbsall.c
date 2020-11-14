031eaa839e81e3b7c63efa0cca4dffecef7dd870
IronBlood <aplshenyang@gmail.com>
2020-11-14 19:53:26 +0800

    format code

--- nju09/www/bbsall.c
+++ nju09/www/bbsall.c
@@ -1,8 +1,7 @@
 #include "bbslib.h"
 
-int
-bbsall_main()
-{	//modify by mintbaggio 20040829 for new www
+int bbsall_main() {
+	//modify by mintbaggio 20040829 for new www
 	struct boardmem *(data[MAXBOARD]), *x;
 	int sortmode, i, total = 0;
 	char bmbuf[IDLEN * 4 + 4];
@@ -24,34 +23,30 @@ bbsall_main()
 	}
 	switch (sortmode) {
 	case 1:
-		qsort(data, total, sizeof (struct boardmem *),
-		      (void *) cmpboard);
+		qsort(data, total, sizeof (struct boardmem *), (void *) cmpboard);
 		break;
 	case 2:
-		qsort(data, total, sizeof (struct boardmem *),
-		      (void *) cmpboardscore);
+		qsort(data, total, sizeof (struct boardmem *), (void *) cmpboardscore);
 		break;
 	case 3:
-		qsort(data, total, sizeof (struct boardmem *),
-		      (void *) cmpboardinboard);
+		qsort(data, total, sizeof (struct boardmem *), (void *) cmpboardinboard);
 		break;
 	}
 	printf("<body><center>\n");
 	printf("<div class=rhead>%s -- 全部讨论区 [讨论区数: <span class=h11>%d</span>]</div><hr>", BBSNAME, total);
 	printf("<table>\n");
-	printf
-	    ("<tr><td>序号</td><td><a href=bbsall?sortmode=1>讨论区名称</a></td><td>V</td><td><nobr>&nbsp;类别&nbsp;</nobr></td><td>中文描述</td><td>版主</td><td><a href=bbsall?sortmode=2>人气</td><td><a href=bbsall?sortmode=3>在线</td></tr>\n");
+	printf("<tr><td>序号</td><td><a href=bbsall?sortmode=1>讨论区名称</a></td><td>V</td><td><nobr>&nbsp;类别&nbsp;</nobr></td><td>中文描述</td><td>版主</td><td><a href=bbsall?sortmode=2>人气</td><td><a href=bbsall?sortmode=3>在线</td></tr>\n");
 	for (i = 0; i < total; i++) {
 		printf("<tr><td>%d</td>", i + 1);
 		printf("<td><a href=%s%s>%s</a></td>",
-		       showByDefMode(), data[i]->header.filename, data[i]->header.filename);
+				showByDefMode(), data[i]->header.filename, data[i]->header.filename);
 		printf("<td>");
 		if (data[i]->header.flag & VOTE_FLAG)
 			printf("<font color=red>V</font>");
 		printf("</td>");
 		printf("<td>[%4.4s]</td>", data[i]->header.type);
 		printf("<td><a href=%s%s>%s</a></td>",
-		       showByDefMode(), data[i]->header.filename, data[i]->header.title);
+				showByDefMode(), data[i]->header.filename, data[i]->header.title);
 		bm2str(bmbuf, &(data[i]->header));
 		if (strlen(bmbuf) == 0)
 			printf("<td>诚征版主中</td>");
@@ -66,3 +61,4 @@ bbsall_main()
 	http_quit();
 	return 0;
 }
+
