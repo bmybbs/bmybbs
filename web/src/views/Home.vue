14fb0d86bdff36bafee3722c0352ba95d9928ede
IronBlood <aplshenyang@gmail.com>
2020-11-23 14:47:38 +0800

    Home 依据屏幕宽度切换 PC 版、mobile 版登录界面

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,14 +1,37 @@
 <template>
-	<Login />
+	<LoginMobile v-if="is_mobile" />
+	<Login v-else />
 </template>
 
 <script>
 import Login from "@/components/Login.vue";
+import LoginMobile from "@/components/LoginMobile.vue";
 
 export default {
+	data() {
+		return {
+			is_mobile: false,
+		};
+	},
+	created() {
+		this.check_width();
+		window.addEventListener("resize", this.resize_handler);
+	},
+	unmounted() {
+		window.removeEventListener("resize", this.resize_handler);
+	},
 	components: {
-		Login
-	}
+		Login,
+		LoginMobile,
+	},
+	methods: {
+		check_width() {
+			this.is_mobile = (window.innerWidth <= 600);
+		},
+		resize_handler() {
+			this.check_width();
+		}
+	},
 };
 </script>
 
