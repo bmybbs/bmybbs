17c1e56e5f174cdce026c08509832c60215e8357
IronBlood <aplshenyang@gmail.com>
2020-11-25 23:16:34 +0800

    用于显示导读页通知、美文的模板初步实现

---
+++ web/src/components/DashboardCommend.vue
@@ -0,0 +1,18 @@
+<template>
+	<ul>
+		<li v-for="article in _articles" :key="article.aid">
+			{{ article.title }} By {{ article.author }} at {{ article.board }}
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	data() {
+	},
+	props: {
+		_articles: Array,
+	},
+}
+</script>
+
