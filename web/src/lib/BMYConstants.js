b228465d0d14ab139fe153f590de6a0a0f30355a
IronBlood <aplshenyang@gmail.com>
2020-11-27 17:40:36 +0800

    新增 BMYSECSTRS 常量

---
+++ web/src/lib/BMYConstants.js
@@ -0,0 +1,18 @@
+export const BMYSECSTRS = [
+	{ id: '0', name: "本站系统", icon: "paper-plane" },
+	{ id: '1', name: "交通大学", icon: "graduation-cap" },
+	{ id: '2', name: "开发技术", icon: "code" },
+	{ id: '3', name: "电脑应用", icon: "laptop" },
+	{ id: '4', name: "学术科学", icon: "atom" },
+	{ id: '5', name: "社会科学", icon: "globe-asia" },
+	{ id: '6', name: "文学艺术", icon: "book" },
+	{ id: '7', name: "知性感性", icon: "heart" },
+	{ id: '8', name: "体育运动", icon: "futbol" },
+	{ id: '9', name: "休闲音乐", icon: "music" },
+	{ id: 'G', name: "游戏天地", icon: "gamepad" },
+	{ id: 'N', name: "新闻信息", icon: "info-circle" },
+	{ id: 'H', name: "乡音乡情", icon: "map" },
+	{ id: 'A', name: "校务信息", icon: "building" },
+	{ id: 'C', name: "俱乐部区", icon: "pizza-slice" }
+];
+
