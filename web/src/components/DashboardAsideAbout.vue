f8b49eef3dd62534ea64ce049a9bfe601fa234ab
IronBlood <aplshenyang@gmail.com>
2020-11-27 22:43:44 +0800

    新增一些基于 bootstrap card 的 dashboard 组件实现

---
+++ web/src/components/DashboardAsideAbout.vue
@@ -0,0 +1,34 @@
+<template>
+	<section class="card bmy-dashboard-card">
+		<div class="card-header">
+			关于 BMY
+		</div>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">CPU: Intel® Xeon® E5-2620 2.1GHz ×4</li>
+			<li class="list-group-item">RAM: 16GB ECC</li>
+			<li class="list-group-item">HD: SAN 4000G</li>
+			<li class="list-group-item">网卡: 双1000Mbps NIC</li>
+		</ul>
+	</section>
+	<section class="card bmy-dashboard-card">
+		<div class="card-header">
+			管理团队
+		</div>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">系统管理员(SysOp): lanboy</li>
+			<li class="list-group-item">顾问团：owl hkmj michaelchal afraid idealism snowriver walkman dianfeng binzhen hullll maxif</li>
+			<li class="list-group-item">站务总站长：beixiansheng</li>
+			<li class="list-group-item">站务副站长：shuishenwy</li>
+			<li class="list-group-item">主管站长： neddy hanson alexaustin IronBlood jxchz changy kiyomi</li>
+			<li class="list-group-item">实习站长：</li>
+
+			<li class="list-group-item">常务管理：beixiansheng</li>
+			<li class="list-group-item">版主管理：shuishenwy</li>
+			<li class="list-group-item">版面管理：neddy changy kiyomi</li>
+			<li class="list-group-item">程序维护：IronBlood</li>
+			<li class="list-group-item">宣传外联：hanson jxchz changy</li>
+			<li class="list-group-item">美工设计：alexaustin</li>
+		</ul>
+	</section>
+</template>
+
---
+++ web/src/components/DashboardSection.vue
@@ -0,0 +1,27 @@
+<template>
+	<section class="card bmy-dashboard-card">
+		<div class="card-header">
+			{{ _name }}
+		</div>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">Cras justo odio</li>
+			<li class="list-group-item">Dapibus ac facilisis in</li>
+			<li class="list-group-item">Vestibulum at eros</li>
+		</ul>
+		<div class="card-footer">
+			热门版面：foo bar baz ...
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data() {
+		return { };
+	},
+	props: {
+		_name: String,
+	},
+}
+</script>
+
