a648f4b42900162f7f57447781743c444671ba1d
IronBlood <aplshenyang@gmail.com>
2020-11-27 22:45:43 +0800

    调整导读页布局

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,22 +1,39 @@
 <template>
-	<h2>兵马俑导读</h2>
+	<div class="container-fluid">
+		<h2>兵马俑导读</h2>
+		<div class="row">
+			<div class="col-md-12 col-lg-9">
 
-	<h3>美文推荐</h3>
-	<DashboardCommend v-bind:_articles="commend" />
+				<DashboardCommend v-bind:_name="'美文推荐'" v-bind:_articles="commend" />
 
-	<h3>通知公告</h3>
-	<DashboardCommend v-bind:_articles="announce" />
+				<DashboardCommend v-bind:_name="'通知公告'" v-bind:_articles="announce" />
+
+				<DashboardSection v-for="section in sections" v-bind:key="section.name" v-bind:_name="section.name" />
+			</div>
+			<aside class="col-md-12 col-lg-3">
+				<DashboardAsideAd />
+				<DashboardAsideRecommend />
+				<DashboardAsideAbout />
+			</aside>
+		</div>
+	</div>
 </template>
 
 <script>
 import { BMYClient } from "@/lib/BMYClient.js"
+import { BMYSECSTRS } from "@/lib/BMYConstants.js"
 import DashboardCommend from "@/components/DashboardCommend.vue"
+import DashboardAsideAd from "@/components/DashboardAsideAd.vue"
+import DashboardAsideRecommend from "@/components/DashboardAsideRecommend.vue"
+import DashboardAsideAbout from "@/components/DashboardAsideAbout.vue"
+import DashboardSection from "@/components/DashboardSection.vue"
 
 export default {
 	data() {
 		return {
 			announce: [],
 			commend: [],
+			sections: BMYSECSTRS,
 		}
 	},
 	mounted() {
@@ -34,7 +51,17 @@ export default {
 	},
 	components: {
 		DashboardCommend,
+		DashboardAsideAd,
+		DashboardAsideRecommend,
+		DashboardAsideAbout,
+		DashboardSection,
 	},
 };
 </script>
+
+<style>
+.bmy-dashboard-card {
+	margin-bottom: 30px;
+}
+</style>
 
