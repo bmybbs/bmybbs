302059cf4905ef7dd7ee873e0d40f097da8f594a
IronBlood <aplshenyang@gmail.com>
2020-11-27 17:41:21 +0800

    导入一些 fontawesome 图标

--- web/src/plugins/font-awesome.js
+++ web/src/plugins/font-awesome.js
@@ -1,8 +1,51 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faLock, faUser } from "@fortawesome/free-solid-svg-icons"
+import {
+	faAtom,
+	faBook,
+	faBuilding,
+	faChartLine,
+	faCode,
+	faCogs,
+	faFutbol,
+	faGamepad,
+	faGlobeAsia,
+	faGraduationCap,
+	faHeart,
+	faInfoCircle,
+	faLaptop,
+	faLock,
+	faMap,
+	faMusic,
+	faPaperPlane,
+	faPizzaSlice,
+	faRss,
+	faShareAlt,
+	faSlidersH,
+	faUser,
+} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
+library.add(faAtom);
+library.add(faBook);
+library.add(faBuilding);
+library.add(faChartLine);
+library.add(faCode);
+library.add(faCogs);
+library.add(faFutbol);
+library.add(faGamepad);
+library.add(faGlobeAsia);
+library.add(faGraduationCap);
+library.add(faHeart);
+library.add(faInfoCircle);
+library.add(faLaptop);
 library.add(faLock);
+library.add(faMap);
+library.add(faMusic);
+library.add(faPaperPlane);
+library.add(faPizzaSlice);
+library.add(faRss);
+library.add(faShareAlt);
+library.add(faSlidersH);
 library.add(faUser);
 
 export {
