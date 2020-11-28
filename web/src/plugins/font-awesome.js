74295ff0fb7780f0d16b4d9838ba2c3cbcac5e55
IronBlood <aplshenyang@gmail.com>
2020-11-28 18:36:57 +0800

    订阅的界面原型

--- web/src/plugins/font-awesome.js
+++ web/src/plugins/font-awesome.js
@@ -1,5 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
 import {
+	faAlignJustify,
 	faAt,
 	faAtom,
 	faBook,
@@ -21,6 +22,7 @@ import {
 	faMusic,
 	faPaperPlane,
 	faPizzaSlice,
+	faRedo,
 	faRss,
 	faShareAlt,
 	faSlidersH,
@@ -28,6 +30,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
+library.add(faAlignJustify);
 library.add(faAt);
 library.add(faAtom);
 library.add(faBook);
@@ -49,6 +52,7 @@ library.add(faMap);
 library.add(faMusic);
 library.add(faPaperPlane);
 library.add(faPizzaSlice);
+library.add(faRedo);
 library.add(faRss);
 library.add(faShareAlt);
 library.add(faSlidersH);
