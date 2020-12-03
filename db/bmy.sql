1cd8cc2fd261b3fb62377d245f624c330330a7ac
IronBlood <aplshenyang@gmail.com>
2020-12-03 11:00:14 +0800

    添加分区表

--- db/bmy.sql
+++ db/bmy.sql
@@ -2,6 +2,12 @@
 -- tables
 --
 
+CREATE TABLE `t_sections` (
+	`id` char(1) NOT NULL COMMENT '这里直接使用一个字符保存分区的编号，同时作为主键，例如 A',
+	`name` char(16) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
 CREATE TABLE `t_users` (
 	`usernum` int NOT NULL COMMENT '对应于在 PASSWDS 文件中的索引，从 1 开始计数',
 	`userid` varchar(14) NOT NULL,
@@ -50,6 +56,29 @@ CREATE TABLE `t_user_subscriptions` (
 	CONSTRAINT `fk_sub_board` FOREIGN KEY (`boardnum`) REFERENCES `t_boards` (`boardnum`),
 	CONSTRAINT `fk_sub_user` FOREIGN KEY (`usernum`) REFERENCES `t_users` (`usernum`) ON DELETE CASCADE
 );
+
+--
+-- data
+--
+
+INSERT INTO `t_sections`
+	(`id`, `name`)
+VALUES
+	("0", "本站系统"),
+	("1", "交通大学"),
+	("2", "开发技术"),
+	("3", "电脑应用"),
+	("4", "学术科学"),
+	("5", "社会科学"),
+	("6", "文学艺术"),
+	("7", "知性感性"),
+	("8", "体育运动"),
+	("9", "休闲音乐"),
+	("G", "游戏天地"),
+	("N", "新闻信息"),
+	("H", "乡音乡情"),
+	("A", "校务信息"),
+	("C", "俱乐部区");
 
 --
 -- procedures
