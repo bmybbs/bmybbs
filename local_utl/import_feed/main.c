fd210b35c35e7556046d3e105a231081979c683d
IronBlood <aplshenyang@gmail.com>
2020-12-05 12:36:16 +0800

    重构: 改变命令行参数

--- local_utl/import_feed/main.c
+++ local_utl/import_feed/main.c
@@ -135,10 +135,13 @@ static int count_threads_of_board(const char *boardname) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argc > 3) {
-		if (strcmp(argv[1], "test") == 0 && strcmp(argv[2], "board") == 0) {
-			count_threads_of_board(argv[3]);
+	if (argc > 1) {
+		if (strcmp(argv[1], "test_board") == 0) {
+			count_threads_of_board(argv[2]);
 		}
+	} else {
+		fprintf(stderr, "[usage] <program_name> <option>\noption:\n");
+		fprintf(stderr, "\ttest_board <boardname>: 测试版面主题计数\n");
 	}
 	return 0;
 }
