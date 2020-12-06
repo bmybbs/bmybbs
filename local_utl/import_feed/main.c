cf182160e4c047183c7a7a9a25e4ae933081c29f
IronBlood <aplshenyang@gmail.com>
2020-12-06 17:07:26 +0800

    新增导入用户和收藏夹数据

--- local_utl/import_feed/main.c
+++ local_utl/import_feed/main.c
@@ -3,6 +3,7 @@
 
 extern int count_threads_of_board(const char *boardname);
 extern int import_board(void);
+extern int import_user(void);
 
 int main(int argc, char *argv[]) {
 	if (argc > 1) {
@@ -12,10 +13,14 @@ int main(int argc, char *argv[]) {
 		if (strcmp(argv[1], "import_board") == 0) {
 			import_board();
 		}
+		if (strcmp(argv[1], "import_user") == 0) {
+			import_user();
+		}
 	} else {
 		fprintf(stderr, "[usage] <program_name> <option>\noption:\n");
 		fprintf(stderr, "\ttest_board <boardname>: 测试版面主题计数\n");
-		fprintf(stderr, "\timport_board: 导入版面信息到 t_boards 表\n");
+		fprintf(stderr, "\timport_board: 导入版面信息到 t_boards 表，并创建版面视图\n");
+		fprintf(stderr, "\timport_user: 导入用户信息到 t_users 表，并初始化订阅元信息，构建对应视图\n");
 	}
 	return 0;
 }
