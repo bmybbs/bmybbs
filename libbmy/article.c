f6c5f7cfbc6bec9da9547b9f32feb43088770ff8
IronBlood <aplshenyang@gmail.com>
2020-12-13 17:53:17 +0800

    添加、删除评论计数的接口

--- libbmy/article.c
+++ libbmy/article.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include "bmy/mysql_wrapper.h"
 #include "bmy/convcode.h"
 #include "bmy/article.h"
 
@@ -21,5 +22,31 @@ void copy_to_utf_header(struct fileheader_utf *dest, struct fileheader *src) {
 	g2u(src->title, len, dest->title, 120);
 
 	dest->count = 1; // the first one!
+}
+
+static void bmy_article_comment_internal(int boardnum, time_t tid, int delta) {
+	const char *sql = "CALL procedure_update_thread_comments(?, ?, ?)";
+	MYSQL_BIND params[3];
+
+	memset(params, 0, sizeof(params));
+	params[0].buffer_type = MYSQL_TYPE_LONG;
+	params[0].buffer = &boardnum;
+	params[0].buffer_length = sizeof(int);
+	params[1].buffer_type = MYSQL_TYPE_LONGLONG;
+	params[1].buffer = &tid;
+	params[1].buffer_length = sizeof(time_t);
+	params[2].buffer_type = MYSQL_TYPE_LONG;
+	params[2].buffer = &delta;
+	params[2].buffer_length = sizeof(int);
+
+	execute_prep_stmt(sql, MYSQL_CHARSET_UTF8, params, NULL, NULL, NULL);
+}
+
+void bmy_article_add_comment(int boardnum, time_t tid) {
+	bmy_article_comment_internal(boardnum, tid, 1);
+}
+
+void bmy_article_del_comment(int boardnum, time_t tid) {
+	bmy_article_comment_internal(boardnum, tid, -1);
 }
 
