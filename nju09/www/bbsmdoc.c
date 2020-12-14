116204e5109dd90147cecac563c272e529a0e7b8
IronBlood <aplshenyang@gmail.com>
2020-12-14 23:38:20 +0800

    format code

--- nju09/www/bbsmdoc.c
+++ nju09/www/bbsmdoc.c
@@ -1,5 +1,9 @@
 #include "bbslib.h"
 
+/* bbsdoc.c */
+extern void nosuchboard(char *board, char *cginame);
+extern void printdocform(char *cginame, char *board);
+
 int
 bbsmdoc_main()
 {
@@ -30,7 +34,7 @@ bbsmdoc_main()
 	html_header(1);
 	check_msg();
 	printf("<script src=/function.js></script>\n");
-	
+
 	fp = fopen(dir, "r");
 	if (fp == 0)
 		http_fatal("错误的讨论区目录");
@@ -42,7 +46,7 @@ bbsmdoc_main()
 		start = 0;
 	printf("<nobr><center>\n");
 	printf("%s -- [讨论区: %s] 版主[%s] 文章数[%d]<hr>\n",
-	       BBSNAME, board, userid_str(bm2str(bmbuf, &(x1->header))), total);
+			BBSNAME, board, userid_str(bm2str(bmbuf, &(x1->header))), total);
 	if (total <= 0) {
 		fclose(fp);
 		http_fatal("本讨论区目前没有文章");
@@ -57,41 +61,31 @@ bbsmdoc_main()
 			break;
 		sprintf(filename, "boards/%s/%s", board, fh2fname(&x));
 		printf("<tr><td>%d", start + i + 1);
-		printf
-		    ("<td><input style='height:18px' name=box%s type=checkbox>",
-		     fh2fname(&x));
-		printf("<td>%s<td>%s", flag_str_bm(x.accessed),
-		       userid_str(fh2owner(&x)));
+		printf("<td><input style='height:18px' name=box%s type=checkbox>", fh2fname(&x));
+		printf("<td>%s<td>%s", flag_str_bm(x.accessed), userid_str(fh2owner(&x)));
 		printf("<td>%12.12s", ytht_ctime(x.filetime) + 4);
 		x.title[40] = 0;
 		printf("<td><a href=con?B=%s&F=%s&N=%d&T=%ld>%s%s </a>\n",
-		       board, fh2fname(&x), start + i + 1, feditmark(x),
-		       strncmp(x.title, "Re: ", 4) ? "○ " : "",
-		       void1(titlestr(x.title)));
+				board, fh2fname(&x), start + i + 1, feditmark(x),
+				strncmp(x.title, "Re: ", 4) ? "○ " : "",
+				void1(titlestr(x.title)));
 	}
 	fclose(fp);
 	printf("</table>\n");
 	printf("<input type=hidden name=mode value=''>\n");
 	printf("<input type=hidden name=board value='%s'>\n", board);
-	printf
-	    ("<input type=button value=加删除标记 onclick="
-	     "'if(confirm(\"请使用telnet方式进行区段删除，这里仅增加删除标记，你真的要加标记吗?\")) {document.form1.mode.value=1; document.form1.submit();}'>&nbsp;&nbsp;\n");
-	printf
-	    ("<input type=button value=加M onclick='document.form1.mode.value=2; document.form1.submit();'>\n");
-	printf
-	    ("<input type=button value=加G onclick='document.form1.mode.value=3; document.form1.submit();'>\n");
-	printf
-	    ("<input type=button value=不可Re onclick='document.form1.mode.value=4; document.form1.submit();'>\n");
-	printf
-	    ("<input type=button value=清除MG onclick='document.form1.mode.value=5; document.form1.submit();'>\n");
+	printf("<input type=button value=加删除标记 onclick="
+			"'if(confirm(\"请使用telnet方式进行区段删除，这里仅增加删除标记，你真的要加标记吗?\")) {document.form1.mode.value=1; document.form1.submit();}'>&nbsp;&nbsp;\n");
+	printf("<input type=button value=加M onclick='document.form1.mode.value=2; document.form1.submit();'>\n");
+	printf("<input type=button value=加G onclick='document.form1.mode.value=3; document.form1.submit();'>\n");
+	printf("<input type=button value=不可Re onclick='document.form1.mode.value=4; document.form1.submit();'>\n");
+	printf("<input type=button value=清除MG onclick='document.form1.mode.value=5; document.form1.submit();'>\n");
 	printf("</form>\n");
 	if (start > 0) {
-		printf("<a href=bbsmdoc?board=%s&start=%d>上一页</a> ",
-		       board, start < 20 ? 0 : start - 20);
+		printf("<a href=bbsmdoc?board=%s&start=%d>上一页</a> ", board, start < 20 ? 0 : start - 20);
 	}
 	if (start < total - 20) {
-		printf("<a href=bbsmdoc?board=%s&start=%d>下一页</a> ",
-		       board, start + 20);
+		printf("<a href=bbsmdoc?board=%s&start=%d>下一页</a> ", board, start + 20);
 	}
 	printf("<a href=bbsdoc?board=%s>一般模式</a> ", board);
 	printf("<a href=bbsdenyall?board=%s>封人名单</a> ", board);
@@ -100,3 +94,4 @@ bbsmdoc_main()
 	http_quit();
 	return 0;
 }
+
