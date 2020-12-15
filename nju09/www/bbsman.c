385880d1b4cda30b34a0591df5be28322ee56e62
IronBlood <aplshenyang@gmail.com>
2020-12-15 13:21:01 +0800

    format code

--- nju09/www/bbsman.c
+++ nju09/www/bbsman.c
@@ -1,5 +1,8 @@
 #include "bbslib.h"
 
+static int do_del(char *board, char *file);
+static int do_set(char *dirptr, int size, char *file, int flag, char *board);
+
 int
 bbsman_main()
 {
@@ -35,8 +38,7 @@ bbsman_main()
 	if (fd < 0)
 		http_fatal("空讨论区");
 	MMAP_TRY {
-		data =
-		    mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 		close(fd);
 		if (data == (void *) -1) {
 			MMAP_UNTRY;
@@ -48,19 +50,16 @@ bbsman_main()
 				total++;
 				if (mode == 1) {
 #ifndef WWW_BM_DO_DEL
-					do_set(data, size, parm_name[i] + 3,
-					       FH_DEL, board);
+					do_set(data, size, parm_name[i] + 3, FH_DEL, board);
 #else
 					do_del(board,parm_name[i]+3);
 #endif
 					cbuf = "delete";
 				} else if (mode == 2) {
-					do_set(data, size, parm_name[i] + 3,
-					       FH_MARKED, board);
+					do_set(data, size, parm_name[i] + 3, FH_MARKED, board);
 					cbuf = "mark";
 				} else if (mode == 3) {
-					do_set(data, size, parm_name[i] + 3,
-					       FH_DIGEST, board);
+					do_set(data, size, parm_name[i] + 3, FH_DIGEST, board);
 					cbuf = "digest";
 				} else if (mode == 5) {
 					do_set(data, size, parm_name[i] + 3, 0, board);
@@ -79,16 +78,14 @@ bbsman_main()
 	if (total <= 0)
 		printf("请先选定文章<br>\n");
 	snprintf(tbuf, sizeof (tbuf), "WWW batch %s on board %s,total %d",
-		 cbuf, board, total);
+		cbuf, board, total);
 	securityreport(tbuf, tbuf);
 	printf("<br><a href=bbsmdoc?board=%s>返回管理模式</a>", board);
 	http_quit();
 	return 0;
 }
 
-int
-do_del(char *board, char *file)
-{
+static int do_del(char *board, char *file) {
 	FILE *fp;
 	int num = 0, filetime;
 	char path[256], buf[256], dir[256], *id = currentuser.userid;
@@ -107,11 +104,10 @@ do_del(char *board, char *file)
 			cancelpost(board, id, &f, !strcmp(id, f.owner));
 			updatelastpost(board);
 			fclose(fp);
-			printf("<tr><td>%s  <td>标题:%s <td>删除成功.\n",
-			       fh2owner(&f), nohtml(f.title));
+			printf("<tr><td>%s  <td>标题:%s <td>删除成功.\n", fh2owner(&f), nohtml(f.title));
 			snprintf(buf, 256, "%s del %s %s %s",
-				 currentuser.userid, board, fh2owner(&f),
-				 f.title);
+				currentuser.userid, board, fh2owner(&f),
+				f.title);
 			newtrace(buf);
 			return 0;
 		}
@@ -122,9 +118,7 @@ do_del(char *board, char *file)
 	return -1;
 }
 
-int
-do_set(char *dirptr, int size, char *file, int flag, char *board)
-{
+static int do_set(char *dirptr, int size, char *file, int flag, char *board) {
 	char path[256], buf[256];
 	struct fileheader *f;
 	int om, og, nm, ng, filetime;
@@ -139,29 +133,28 @@ do_set(char *dirptr, int size, char *file, int flag, char *board)
 		om = f->accessed & FH_MARKED;
 		og = f->accessed & FH_DIGEST;
 		f->accessed |= flag;
-		if (flag == 0) 
+		if (flag == 0)
 			f->accessed = 0;
 		nm = f->accessed & FH_MARKED;
 		ng = f->accessed & FH_DIGEST;
-		printf("<tr><td>%s<td>标题:%s<td>标记成功.\n",
-		       fh2owner(f), nohtml(f->title));
+		printf("<tr><td>%s<td>标题:%s<td>标记成功.\n", fh2owner(f), nohtml(f->title));
 		buf[0] = 0;
 		if ((!om) && (nm))
 			snprintf(buf, 256, "%s mark %s %s %s",
-				 currentuser.userid, board,
-				 fh2owner(f), f->title);
+				currentuser.userid, board,
+				fh2owner(f), f->title);
 		if ((om) && (!nm))
 			snprintf(buf, 256, "%s unmark %s %s %s",
-				 currentuser.userid, board,
-				 fh2owner(f), f->title);
+				currentuser.userid, board,
+				fh2owner(f), f->title);
 		if ((!og) && (ng))
 			snprintf(buf, 256, "%s digest %s %s %s",
-				 currentuser.userid, board,
-				 fh2owner(f), f->title);
+				currentuser.userid, board,
+				fh2owner(f), f->title);
 		if ((og) && (!ng))
 			snprintf(buf, 256, "%s undigest %s %s %s",
-				 currentuser.userid, board,
-				 fh2owner(f), f->title);
+				currentuser.userid, board,
+				fh2owner(f), f->title);
 		if (buf[0] != 0)
 			newtrace(buf);
 		return 0;
@@ -169,3 +162,4 @@ do_set(char *dirptr, int size, char *file, int flag, char *board)
 	printf("<td><td><td>%s<td>文件不存在.\n", file);
 	return -1;
 }
+
