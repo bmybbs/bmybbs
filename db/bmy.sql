5fc1a84c11f508fe71176e82e4fe642662e61524
IronBlood <aplshenyang@gmail.com>
2020-12-20 20:16:08 +0800

    移除存储过程参数的 IN 关键字（默认值），所有参数使用 _ 作为前缀，和列进行区分

--- db/bmy.sql
+++ db/bmy.sql
@@ -65,166 +65,119 @@ CREATE TABLE `t_user_subscriptions` (
 DELIMITER $$
 
 -- 依据 secstr 创建分区视图
-CREATE PROCEDURE procedure_create_section_view(
-	IN secstr char(1)
-)
+CREATE PROCEDURE procedure_create_section_view(_secstr char(1))
 BEGIN
-	SET @sql = CONCAT("CREATE VIEW v_section_", secstr, " AS SELECT `boardname_en`, `boardname_zh`, `timestamp`, `title`, `author`, `comments`, `accessed` FROM `t_boards`, `t_threads` where `t_boards`.`boardnum` = `t_threads`.`boardnum` and `t_boards`.`secstr` = \"", secstr, "\" order by `timestamp` desc");
+	SET @sql = CONCAT("CREATE VIEW v_section_", _secstr, " AS SELECT `boardname_en`, `boardname_zh`, `timestamp`, `title`, `author`, `comments`, `accessed` FROM `t_boards`, `t_threads` where `t_boards`.`boardnum` = `t_threads`.`boardnum` and `t_boards`.`secstr` = \"", secstr, "\" order by `timestamp` desc");
 	PREPARE stmt FROM @sql;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
 END$$
 
 -- 依据 boardnum, boardname_en 创建视图，使用 call 调用
-CREATE PROCEDURE procedure_create_board_view(
-	IN boardnum int,
-	IN boardname_en varchar(40)
-)
+CREATE PROCEDURE procedure_create_board_view(_boardnum int, _boardname_en varchar(40))
 BEGIN
-	SET @sql = CONCAT("CREATE VIEW v_board_", boardname_en, " AS SELECT `boardname_en`, `boardname_zh`, `timestamp`, `title`, `author`, `comments`, `accessed` FROM `t_boards`, `t_threads` where `t_boards`.`boardnum` = `t_threads`.`boardnum` and `t_threads`.`boardnum` = ", boardnum, " order by `timestamp` desc");
+	SET @sql = CONCAT("CREATE VIEW v_board_", _boardname_en, " AS SELECT `boardname_en`, `boardname_zh`, `timestamp`, `title`, `author`, `comments`, `accessed` FROM `t_boards`, `t_threads` where `t_boards`.`boardnum` = `t_threads`.`boardnum` and `t_threads`.`boardnum` = ", _boardnum, " order by `timestamp` desc");
 	PREPARE stmt FROM @sql;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
 END$$
 
-CREATE PROCEDURE procedure_delete_board_view(
-	IN boardname_en varchar(40)
-)
+CREATE PROCEDURE procedure_delete_board_view(_boardname_en varchar(40))
 BEGIN
-	set @sql = CONCAT("DROP VIEW v_board_", boardname_en);
+	set @sql = CONCAT("DROP VIEW v_board_", _boardname_en);
 	PREPARE stmt FROM @sql;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
 END$$
 
 -- 依据 userid 创建用户订阅视图
-CREATE PROCEDURE procedure_create_feed_view(
-	IN usernum int,
-	IN userid varchar(14)
-)
+CREATE PROCEDURE procedure_create_feed_view(_usernum int, _userid varchar(14))
 BEGIN
-	SET @sql = CONCAT("CREATE VIEW v_feed_", userid, " AS SELECT `boardname_en`, `boardname_zh`, `timestamp`, `title`, `author`, `comments`, `accessed` FROM `t_boards`, `t_threads` where `t_threads`.`boardnum` IN (SELECT boardnum FROM `t_user_subscriptions` WHERE usernum = ", usernum, ") and `t_threads`.`boardnum` = `t_boards`.`boardnum` order by `timestamp` desc");
+	SET @sql = CONCAT("CREATE VIEW v_feed_", _userid, " AS SELECT `boardname_en`, `boardname_zh`, `timestamp`, `title`, `author`, `comments`, `accessed` FROM `t_boards`, `t_threads` where `t_threads`.`boardnum` IN (SELECT boardnum FROM `t_user_subscriptions` WHERE usernum = ", _usernum, ") and `t_threads`.`boardnum` = `t_boards`.`boardnum` order by `timestamp` desc");
 	PREPARE stmt FROM @sql;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
 END$$
 
 -- 插入分区
-CREATE PROCEDURE procedure_insert_section(
-	IN secstr char(1),
-	IN name char(16)
-)
+CREATE PROCEDURE procedure_insert_section(_secstr char(1), _name char(16))
 BEGIN
-	INSERT INTO `t_sections` (`id`, `name`) VALUE (secstr, name);
-	CALL procedure_create_section_view(secstr);
+	INSERT INTO `t_sections` (`id`, `name`) VALUE (_secstr, _name);
+	CALL procedure_create_section_view(_secstr);
 END$$
 
 -- 插入版面
-CREATE PROCEDURE procedure_insert_board(
-	IN boardnum int,
-	IN boardname_en varchar(40),
-	IN boardname_zh varchar(40),
-	IN secstr char(1)
-)
+CREATE PROCEDURE procedure_insert_board(_boardnum int, _boardname_en varchar(40), _boardname_zh varchar(40), _secstr char(1))
 BEGIN
 	INSERT INTO `t_boards`
 		(`boardnum`, `boardname_en`, `boardname_zh`, `secstr`)
 	VALUE
-		(boardnum, boardname_en, boardname_zh, secstr);
-	CALL procedure_create_board_view(boardnum, boardname_en);
+		(_boardnum, _boardname_en, _boardname_zh, _secstr);
+	CALL procedure_create_board_view(_boardnum, _boardname_en);
 END$$
 
 -- 删除版面
-CREATE PROCEDURE procedure_delete_board(
-	IN boardnum int,
-	IN boardname_en varchar(40)
-)
+CREATE PROCEDURE procedure_delete_board(_boardnum int, _boardname_en varchar(40))
 BEGIN
-	CALL procedure_delete_board_view(boardname_en);
-	DELETE FROM `t_boards` WHERE `boardnum` = boardnum;
+	CALL procedure_delete_board_view(_boardname_en);
+	DELETE FROM `t_boards` WHERE `boardnum` = _boardnum;
 END$$
 
 -- 插入用户
-CREATE PROCEDURE procedure_insert_user(
-	IN usernum int,
-	IN userid varchar(14)
-)
-BEGIN
-	INSERT INTO `t_users`(`usernum`, `userid`) VALUE(usernum, userid);
-	INSERT INTO `t_feed_meta`(`usernum`) VALUE(usernum);
-	CALL procedure_create_feed_view(usernum, userid);
+CREATE PROCEDURE procedure_insert_user(_usernum int, _userid varchar(14)
+) BEGIN
+	INSERT INTO `t_users`(`usernum`, `userid`) VALUE(_usernum, _userid);
+	INSERT INTO `t_feed_meta`(`usernum`) VALUE(_usernum);
+	CALL procedure_create_feed_view(_usernum, _userid);
 END$$
 
 -- 删除用户，通过外键层级删除订阅关系和订阅元数据
-CREATE PROCEDURE procedure_delete_user(
-	IN usernum int,
-	IN userid varchar(14)
-)
+CREATE PROCEDURE procedure_delete_user(_usernum int, _userid varchar(14))
 BEGIN
-	SET @sql = CONCAT("DROP VIEW v_feed_", userid);
+	SET @sql = CONCAT("DROP VIEW v_feed_", _userid);
 	PREPARE stmt FROM @sql;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
-	DELETE FROM `t_users` where `t_users`.`usernum` = usernum;
+	DELETE FROM `t_users` where `t_users`.`usernum` = _usernum;
 END$$
 
 -- 更新版面
 -- TODO 使用条件判断语句？不过重命名版面、移动分区的这个事情不常见
-CREATE PROCEDURE procedure_update_board(
-	IN boardnum int,
-	IN new_boardname_en varchar(40),
-	IN new_boardname_zh varchar(40),
-	IN new_secstr char(1)
-)
+CREATE PROCEDURE procedure_update_board(_boardnum int, _new_boardname_en varchar(40), _new_boardname_zh varchar(40), _new_secstr char(1))
 BEGIN
-	SELECT `boardname_en` INTO @old_boardname_en FROM `t_boards` WHERE `t_boards`.`boardnum` = boardnum LIMIT 1;
+	SELECT `boardname_en` INTO @old_boardname_en FROM `t_boards` WHERE `t_boards`.`boardnum` = _boardnum LIMIT 1;
 	CALL procedure_delete_board_view(@old_boardname_en);
-	CALL procedure_create_board_view(boardnum, new_boardname_en);
+	CALL procedure_create_board_view(_boardnum, _new_boardname_en);
 
-	UPDATE `t_boards` SET `boardname_en` = new_boardname_en, `boardname_zh` = new_boardname_zh, `secstr` = new_secstr WHERE `t_boards`.`boardnum` = boardnum;
+	UPDATE `t_boards` SET `boardname_en` = _new_boardname_en, `boardname_zh` = _new_boardname_zh, `secstr` = _new_secstr WHERE `t_boards`.`boardnum` = _boardnum;
 END$$
 
 -- 更新主题评论数
 -- +1 / -1 在代码调用中设定
-CREATE PROCEDURE procedure_update_thread_comments(
-	IN boardnum int,
-	IN timestamp int,
-	IN delta int
-)
+CREATE PROCEDURE procedure_update_thread_comments(_boardnum int, _timestamp bigint, _delta int)
 BEGIN
-	UPDATE `t_threads` SET `comments` = `comments` + delta
-	WHERE `boardnum` = boardnum AND `timestamp` = timestamp;
+	UPDATE `t_threads` SET `comments` = `comments` + _delta
+	WHERE `boardnum` = _boardnum AND `timestamp` = _timestamp;
 END$$
 
 -- 更新主题标题
-CREATE PROCEDURE procedure_update_thread_title(
-	IN boardnum int,
-	IN timestamp int,
-	IN title varchar(120)
-)
+CREATE PROCEDURE procedure_update_thread_title(_boardnum int, _timestamp bigint, _title varchar(120))
 BEGIN
-	UPDATE `t_threads` SET `title` = title
-	WHERE `boardnum` = boardnum AND `timestamp` = timestamp;
+	UPDATE `t_threads` SET `title` = _title
+	WHERE `boardnum` = _boardnum AND `timestamp` = _timestamp;
 END$$
 
 -- 更新主题标记
-CREATE PROCEDURE procedure_update_thread_accessed(
-	IN boardnum int,
-	IN timestamp int,
-	IN accessed int
-)
+CREATE PROCEDURE procedure_update_thread_accessed(_boardnum int, _timestamp bigint, _accessed int)
 BEGIN
-	UPDATE `t_threads` SET `accessed` = accessed
-	WHERE `boardnum` = boardnum AND `timestamp` = timestamp;
+	UPDATE `t_threads` SET `accessed` = _accessed
+	WHERE `boardnum` = _boardnum AND `timestamp` = _timestamp;
 END$$
 
 -- 删除主题
-CREATE PROCEDURE procedure_delete_thread(
-	IN boardnum int,
-	IN timestamp int
-)
+CREATE PROCEDURE procedure_delete_thread(_boardnum int, _timestamp bigint)
 BEGIN
-	DELETE FROM `t_threads` where `boardnum` = boardnum AND `timestamp` = timestamp;
+	DELETE FROM `t_threads` where `boardnum` = _boardnum AND `timestamp` = _timestamp;
 END$$
 
 DELIMITER ;
