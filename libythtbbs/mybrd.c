20687bbada2ca351a20cf8e731f9be7cd1722174
IronBlood <aplshenyang@gmail.com>
2020-12-22 21:55:19 +0800

    给收藏夹模块新增一些扩展接口

--- libythtbbs/mybrd.c
+++ libythtbbs/mybrd.c
@@ -10,6 +10,7 @@
 static const char *GOODBRD = ".goodbrd";
 
 static void ythtbbs_mybrd_sync(const char *userid, ythtbbs_mybrd_has_read_perm func);
+static void ythtbbs_mybrd_sync_ext(const struct user_info *ptr_info, ythtbbs_mybrd_has_read_perm_ext func);
 
 void ythtbbs_mybrd_load(const char *userid, struct goodboard *mybrd, ythtbbs_mybrd_has_read_perm func) {
 	char buf[STRLEN];
@@ -26,6 +27,39 @@ void ythtbbs_mybrd_load(const char *userid, struct goodboard *mybrd, ythtbbs_myb
 
 			ytht_strsncpy(mybrd->ID[mybrd->num], ytht_strtrim(buf), sizeof(mybrd->ID[mybrd->num]));
 			if (func(userid, mybrd->ID[mybrd->num])) {
+				mybrd->num = mybrd->num + 1;
+			}
+		}
+
+		fclose(fp);
+	}
+
+	if (mybrd->num == 0) {
+		// 这里重构时就省略 currboard 步骤了
+		// 如果按照文档安装程序，DEFAULTBOARD 也就是 "sysop" 版面是默认存在的
+		// 如果变更了 DEFAULTBOARD，请提前在 .BOARDS 中添加对应的版面
+		if (ythtbbs_cache_Board_get_board_by_name(DEFAULTBOARD)) {
+			strcpy(mybrd->ID[0], DEFAULTBOARD);
+			mybrd->num = 1;
+		}
+	}
+}
+
+void ythtbbs_mybrd_load_ext(const struct user_info *ptr_info, struct goodboard *mybrd, ythtbbs_mybrd_has_read_perm_ext func) {
+	char buf[STRLEN];
+	FILE *fp;
+
+	if (func == NULL)
+		return;
+
+	sethomefile_s(buf, sizeof(buf), ptr_info->userid, GOODBRD);
+	if ((fp = fopen(buf, "r"))) {
+		for (mybrd->num = 0; mybrd->num < GOOD_BRD_NUM;) {
+			if (!fgets(buf, sizeof(buf), fp))
+				break;
+
+			ytht_strsncpy(mybrd->ID[mybrd->num], ytht_strtrim(buf), sizeof(mybrd->ID[mybrd->num]));
+			if (func(ptr_info, mybrd->ID[mybrd->num])) {
 				mybrd->num = mybrd->num + 1;
 			}
 		}
@@ -70,6 +104,35 @@ int ythtbbs_mybrd_save(const char *userid, struct goodboard *mybrd, ythtbbs_mybr
 	}
 
 	ythtbbs_mybrd_sync(userid, func);
+	return count;
+}
+
+int ythtbbs_mybrd_save_ext(const struct user_info *ptr_info, struct goodboard *mybrd, ythtbbs_mybrd_has_read_perm_ext func) {
+	int i;
+	int count = 0;
+	char buf[STRLEN];
+	FILE *fp;
+	struct boardmem *x;
+
+	if (func == NULL)
+		return count;
+
+	sethomefile_s(buf, sizeof(buf), ptr_info->userid, GOODBRD);
+	fp = fopen(buf, "w");
+	if (fp) {
+		flock(fileno(fp), LOCK_EX);
+		for (i = 0; i < mybrd->num && i < GOOD_BRD_NUM; i++) {
+			if (!func(ptr_info, mybrd->ID[i]))
+				continue;
+
+			x = ythtbbs_cache_Board_get_board_by_name(mybrd->ID[i]);
+			fprintf(fp, "%s\n", x->header.filename);
+			count++;
+		}
+		fclose(fp);
+	}
+
+	ythtbbs_mybrd_sync_ext(ptr_info, func);
 	return count;
 }
 
@@ -144,6 +207,32 @@ static void ythtbbs_mybrd_sync(const char *userid, ythtbbs_mybrd_has_read_perm f
 		real_num++;
 	}
 	bmy_subscription_sync(1 + ythtbbs_cache_UserIDHashTable_find_idx(userid), bnums, real_num);
+
+	free(bnums);
+}
+
+static void ythtbbs_mybrd_sync_ext(const struct user_info *ptr_info, ythtbbs_mybrd_has_read_perm_ext func) {
+	// 本函数只在 save 里调用，这里暂时不验证 userid 是否存在了...
+	struct goodboard local_mybrd;
+	int *bnums, i, real_num;
+	struct boardmem *b;
+
+	memset(&local_mybrd, 0, sizeof(struct goodboard));
+	ythtbbs_mybrd_load_ext(ptr_info, &local_mybrd, func);
+
+	if (local_mybrd.num == 0)
+		return;
+
+	bnums = calloc(local_mybrd.num, sizeof(int));
+	for (i = 0, real_num = 0; i < local_mybrd.num; i++) {
+		// load 的时候已经校验过读取权限了，因此这里版面应当存在
+		b = ythtbbs_cache_Board_get_board_by_name(local_mybrd.ID[i]);
+		if (bmy_board_is_system_board(b->header.title))
+			continue;
+		bnums[real_num] = 1 + ythtbbs_cache_Board_get_idx_by_ptr(b);
+		real_num++;
+	}
+	bmy_subscription_sync(1 + ythtbbs_cache_UserIDHashTable_find_idx(ptr_info->userid), bnums, real_num);
 
 	free(bnums);
 }
