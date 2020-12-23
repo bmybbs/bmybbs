cf3889e86cef89d8e1e526ba8038a3511d630e13
IronBlood <aplshenyang@gmail.com>
2020-12-24 00:08:46 +0800

    新增弹框示显示版面元数据的控件（基于 Bootstrap Popover，动态创建 vue 组件实例）

---
+++ web/src/components/PopoverBoardInfo.vue
@@ -0,0 +1,82 @@
+<template>
+	<span ref="span" @mouseover="openPopover" @mouseout="closePopover">{{_boardname_zh}}</span>
+</template>
+
+<script>
+import { createApp } from "vue"
+import Popover from "bootstrap/js/dist/popover"
+import { BMYClient } from "@/lib/BMYClient.js"
+import CardBoardInfo from "@/components/CardBoardInfo.vue"
+
+export default {
+	date() {
+		return {
+			internal_id: (new Date().getTime()),
+			status_mouse_on: false,
+			info: null,
+			popover: null,
+			v_instance: null,
+		};
+	},
+	props: {
+		_boardname_en: String,
+		_boardname_zh: String,
+	},
+	methods: {
+		openPopover() {
+			if (!this.status_mouse_on) {
+				this.status_mouse_on = true;
+				if (this.info == null) {
+					BMYClient.get_board_info(this._boardname_en).then(response => {
+						this.info = response;
+						this.doOpen();
+					});
+				} else {
+					this.doOpen();
+				}
+			}
+		},
+		doOpen() {
+			var that = this;
+
+			this.v_instance = createApp(CardBoardInfo, {
+				_boardname_en: that._boardname_en,
+				_boardname_zh: that._boardname_zh,
+				_secstr: that.info.secstr,
+				_article_num: that.info.article_num,
+				_thread_num: that.info.thread_num,
+				_inboard_num: that.info.inboard_num,
+				_today_new: that.info.today_new,
+			});
+
+			this.popover = new Popover(this.$refs.span, {
+				container: "body",
+				placement: "bottom",
+				html: true,
+				content: "<div id='card" + that.internal_id + "'></div>"
+			});
+
+			this.popover.show();
+			this.v_instance.mount("#card" + this.internal_id);
+		},
+		closePopover() {
+			this.status_mouse_on = false;
+
+			if (this.v_instance) {
+				this.v_instance.unmount("#card" + this.internal_id);
+				this.v_instance = null;
+			}
+
+			if (this.popover) {
+				this.popover.dispose();
+				this.popover = null;
+			}
+		},
+	},
+}
+</script>
+
+<style scoped>
+span { display: inline-block; }
+</style>
+
