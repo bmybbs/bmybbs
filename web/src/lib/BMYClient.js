e6c8dd31fa180d3ab0e5448833c4cbf691b89754
IronBlood <aplshenyang@gmail.com>
2020-12-25 21:30:23 +0800

    变更为 template string

--- web/src/lib/BMYClient.js
+++ web/src/lib/BMYClient.js
@@ -13,10 +13,10 @@ export const BMYClient = {
 		return myFetchGet(`/api/article/list?type=board&board=${boardname_en}&btype=${mode}`);
 	},
 	get_board_info(boardname_en) {
-		return myFetchGet("/api/board/info?bname=" + boardname_en);
+		return myFetchGet(`/api/board/info?bname=${boardname_en}`);
 	},
 	get_boards_by_section(secstr, sortmode) {
-		return myFetchGet("/api/board/list?secstr=" + secstr + "&sortmode=" + sortmode);
+		return myFetchGet(`/api/board/list?secstr=${secstr}&sortmode=${sortmode}`);
 	},
 	get_commend() {
 		return myFetchGet("/api/article/list?type=commend");
@@ -25,7 +25,7 @@ export const BMYClient = {
 		return myFetchGet("/api/board/fav/list");
 	},
 	get_feed(start) {
-		return myFetchGet("/api/subscription/list?start=" + start);
+		return myFetchGet(`/api/subscription/list?start=${start}`);
 	},
 	get_sectop(secstr) {
 		return myFetchGet(`/api/article/list?type=sectop&secstr=${secstr}`);
