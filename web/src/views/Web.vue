4df6213809453563065571ec04af38f9ca7c3d93
IronBlood <aplshenyang@gmail.com>
2020-12-27 12:58:17 +0800

    依据 bootstrap 5.0.0-b1 示例更新，修正布局

--- web/src/views/Web.vue
+++ web/src/views/Web.vue
@@ -44,7 +44,7 @@
 					</div>
 				</div>
 			</nav>
-			<main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+			<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 				<router-view />
 			</main>
 		</div>
