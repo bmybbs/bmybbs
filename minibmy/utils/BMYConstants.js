8350ad483ea03f777e9dc1f369ac907c78b9472c
IronBlood <aplshenyang@gmail.com>
2020-12-28 22:17:05 +0800

    重构: 将分区信息移动到常量文件中

--- minibmy/utils/BMYConstants.js
+++ minibmy/utils/BMYConstants.js
@@ -1,2 +1,20 @@
 export const BASE_URL = "https://bbs.xjtu.edu.cn";
 
+export const BMYSECSTRS = [
+	{ id: "0", text: "本站系统" },
+	{ id: "1", text: "交通大学" },
+	{ id: "2", text: "开发技术" },
+	{ id: "3", text: "电脑应用" },
+	{ id: "4", text: "学术科学" },
+	{ id: "5", text: "社会科学" },
+	{ id: "6", text: "文学艺术" },
+	{ id: "7", text: "知性感性" },
+	{ id: "8", text: "体育运动" },
+	{ id: "9", text: "休闲音乐" },
+	{ id: "G", text: "游戏天地" },
+	{ id: "N", text: "新闻信息" },
+	{ id: "H", text: "乡音乡情" },
+	{ id: "A", text: "校务信息" },
+	{ id: "C", text: "俱乐部区" },
+];
+
