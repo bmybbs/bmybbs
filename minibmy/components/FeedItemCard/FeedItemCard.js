c89da9bdf4e695f6c17641099a98179ded453f4d
IronBlood <aplshenyang@gmail.com>
2020-12-29 00:16:32 +0800

    minibmy 新增 feed 卡片式显示（和文章接口略有不同）

---
+++ minibmy/components/FeedItemCard/FeedItemCard.js
@@ -0,0 +1,26 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime"
+import localizedFormat from "dayjs/plugin/localizedFormat"
+import "dayjs/locale/zh-cn"
+
+dayjs.locale("zh-cn");
+dayjs.extend(relativeTime);
+dayjs.extend(localizedFormat);
+
+Component({
+	properties: {
+		article: {
+			type: Object
+		}
+	},
+	data: {
+	},
+	attached: function() {
+		this.setData({
+			readableTime: dayjs(this.data.article.tid * 1000).fromNow(),
+		})
+	},
+	methods: {
+	}
+})
+
