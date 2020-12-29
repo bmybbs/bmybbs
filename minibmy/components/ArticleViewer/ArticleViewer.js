8a129b86976000c87ec90f9b4edf1315ef3149e0
IronBlood <aplshenyang@gmail.com>
2020-12-29 17:08:29 +0800

    minibmy 引入 xmldom 实现 toggleAha 事件（开关 ANSI 样式）

--- minibmy/components/ArticleViewer/ArticleViewer.js
+++ minibmy/components/ArticleViewer/ArticleViewer.js
@@ -1,5 +1,6 @@
 import { BMYClient } from "../../utils/BMYClient.js"
 import { getLongVersionTime } from "../../utils/Time.js"
+import { DOMParser, XMLSerializer } from "xmldom"
 
 Component({
 	properties: {
@@ -20,6 +21,28 @@ Component({
 		},
 	},
 	methods: {
+		toggleAha() {
+			this.setData({
+				show_ansi: !this.data.show_ansi
+			});
+
+			const doc = new DOMParser().parseFromString(this.data.article.content, "text/html");
+			const aha_arr = [].slice.call(doc.getElementsByTagName("span"));
+			aha_arr.map((x) => {
+				let classes = x.getAttribute("class");
+				if (this.data.show_ansi) {
+					x.setAttribute("class", "aha " + classes);
+				} else {
+					x.setAttribute("class", classes.replace("aha ", ""));
+				}
+			});
+
+			let article = this.data.article;
+			article.content = new XMLSerializer().serializeToString(doc);
+			this.setData({
+				article
+			});
+		}
 	},
 })
 
