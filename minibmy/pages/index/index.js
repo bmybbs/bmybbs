9dd1452ea5aa56d9d5398402b1eab75fa717734b
IronBlood <aplshenyang@gmail.com>
2021-01-01 00:31:14 +0800

    minibmy 登录页

--- minibmy/pages/index/index.js
+++ minibmy/pages/index/index.js
@@ -1,4 +1,5 @@
-//index.js
+import { BMYClient } from "../../utils/BMYClient.js"
+import { BMY_EC } from "../../utils/BMYConstants.js"
 //获取应用实例
 const app = getApp()
 
@@ -10,9 +11,46 @@ Page({
 		canIUse: wx.canIUse('button.open-type.getUserInfo')
 	},
 	//事件处理函数
-	bindViewTap: function() {
-		wx.navigateTo({
-			url: '../logs/logs'
+	bindGuest: function() {
+		wx.switchTab({
+			url: '../dashboard/dashboard'
+		})
+	},
+	bindLogin: function() {
+		wx.login({
+			success: res => {
+				BMYClient.oauth_login(res.code).then(response => {
+					if (response.errcode == BMY_EC.API_RT_SUCCESS) {
+						wx.switchTab({
+							url: '../dashboard/dashboard',
+						})
+					} else if (response.errcode == BMY_EC.API_RT_WXAPIERROR) {
+						wx.showToast({
+							title: "微信服务错误",
+							icon: "error",
+							duration: 2000
+						})
+					} else if (response.errcode == BMY_EC.API_RT_NOOPENID) {
+						wx.showModal({
+							title: "提示",
+							content: "当前账户没有关联 BMY 账户，是否立即关联？",
+							cancelText: "游客登录",
+							confirmText: "立即关联",
+							success: function(res) {
+								if (res.confirm) {
+									wx.navigateTo({
+										url: "../bind/step1"
+									})
+								} else {
+									wx.switchTab({
+										url: '../dashboard/dashboard'
+									})
+								}
+							},
+						})
+					}
+				})
+			}
 		})
 	},
 	onLoad: function () {
