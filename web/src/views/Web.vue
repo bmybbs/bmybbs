b8d8a33021faa8abfed8dbd1d8e2af6e5037efdd
IronBlood <aplshenyang@gmail.com>
2021-01-06 17:07:24 +0800

    设置页面最小高度

--- web/src/views/Web.vue
+++ web/src/views/Web.vue
@@ -62,7 +62,7 @@
 					</div>
 				</div>
 			</nav>
-			<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+			<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 min-vh-100">
 				<router-view />
 			</main>
 		</div>
