c260fd50287966e95015ba7728bcf812dd792aca
IronBlood <aplshenyang@gmail.com>
2021-01-07 17:28:09 +0800

    调整微信设置布局，适配移动版屏幕，增加二维码刷新按钮

--- web/src/components/AccordionSettingWechat.vue
+++ web/src/components/AccordionSettingWechat.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="accordion-item">
-		<h2 class="accordion-header">
+		<h2 class="accordion-header bg-white text-dark">
 			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#setting-wechat">微信关联</button>
 		</h2>
-		<div id="setting-wechat" class="accordion-collapse collapse">
+		<div id="setting-wechat" class="accordion-collapse collapse bg-white">
 
 			<div v-if="hasopenid">
 				您已关联微信账号
@@ -26,14 +26,14 @@
 			</div>
 
 			<div v-else>
-				<div class="setting-container">
+				<div class="d-flex flex-column flex-sm-row">
 					<div>
 						<canvas ref="canvas"></canvas>
 					</div>
 					<div>
 						<div class="text">
-							<div>请在微信小程序关联 BMY 账户页扫描左侧二维码，或者手动输入以下代码获取校验码</div>
-							<div class="key">{{msg}}</div>
+							<div>请在微信小程序关联 BMY 账户页扫描<span class="d-none d-sm-inline">左侧</span><span class="d-inline d-sm-none">上方</span>二维码，或者手动输入以下代码获取校验码</div>
+							<div class="font-monospace">{{msg}}</div>
 						</div>
 						<div class="code row">
 							<div class="col-auto">
@@ -46,6 +46,10 @@
 
 							<div class="col-auto">
 								<button class="btn btn-primary" @click="submit">提交</button>
+							</div>
+
+							<div class="col-auto">
+								<button class="btn btn-secondary" @click="load">刷新</button>
 							</div>
 						</div>
 					</div>
@@ -70,19 +74,22 @@ export default {
 		};
 	},
 	mounted() {
-		BMYClient.oauth_get_key().then(response => {
-			if (response.errcode == BMY_EC.API_RT_SUCCESSFUL) {
-				QRCode.toCanvas(this.$refs.canvas, response.key, function(error) {
-					if (error)
-						console.error(error);
-				});
-				this.msg = response.key;
-			} else if (response.errcode == BMY_EC.API_RT_HASOPENID) {
-				this.hasopenid = true;
-			}
-		});
+		this.load();
 	},
 	methods: {
+		load() {
+			BMYClient.oauth_get_key().then(response => {
+				if (response.errcode == BMY_EC.API_RT_SUCCESSFUL) {
+					QRCode.toCanvas(this.$refs.canvas, response.key, function(error) {
+						if (error)
+							console.error(error);
+					});
+					this.msg = response.key;
+				} else if (response.errcode == BMY_EC.API_RT_HASOPENID) {
+					this.hasopenid = true;
+				}
+			});
+		},
 		submit() {
 			BMYClient.oauth_check_code(this.code).then(response => {
 				switch (response.errcode) {
@@ -104,17 +111,8 @@ export default {
 </script>
 
 <style scoped>
-.setting-container {
-	display: flex;
-}
-
 .text, .code {
 	padding-top: 10px;
-}
-
-.key {
-	display: block;
-	font-family: consolas, "Courier New", monospace;
 }
 </style>
 
