b1f029141a839fa101996187f5c808eedfef1709
IronBlood <aplshenyang@gmail.com>
2021-01-15 16:44:55 +0800

    format code

--- src/bbs/userinfo.c
+++ src/bbs/userinfo.c
@@ -1,25 +1,25 @@
 /*
- Pirate Bulletin Board System
- Copyright (C) 1990, Edward Luke, lush@Athena.EE.MsState.EDU
- Eagles Bulletin Board System
- Copyright (C) 1992, Raymond Rocker, rocker@rock.b11.ingr.com
- Guy Vega, gtvega@seabass.st.usm.edu
- Dominic Tynes, dbtynes@seabass.st.usm.edu
- Firebird Bulletin Board System
- Copyright (C) 1996, Hsien-Tsung Chang, Smallpig.bbs@bbs.cs.ccu.edu.tw
- Peng Piaw Foong, ppfoong@csie.ncu.edu.tw
- Copyright (C) 1999, Zhou Lin, kcn@cic.tsinghua.edu.cn
+	Pirate Bulletin Board System
+	Copyright (C) 1990, Edward Luke, lush@Athena.EE.MsState.EDU
+	Eagles Bulletin Board System
+	Copyright (C) 1992, Raymond Rocker, rocker@rock.b11.ingr.com
+						Guy Vega, gtvega@seabass.st.usm.edu
+						Dominic Tynes, dbtynes@seabass.st.usm.edu
+	Firebird Bulletin Board System
+	Copyright (C) 1996, Hsien-Tsung Chang, Smallpig.bbs@bbs.cs.ccu.edu.tw
+						Peng Piaw Foong, ppfoong@csie.ncu.edu.tw
+	Copyright (C) 1999, Zhou Lin, kcn@cic.tsinghua.edu.cn
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 1, or (at your option)
- any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 1, or (at your option)
+	any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- */
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
 
 #include "bbs.h"
 #include "bcache.h"
@@ -622,8 +622,7 @@ void x_fillform()
 		getfield(10, "包括寝室或门牌号码", "目前住址或通讯地址", addr, STRLEN);
 		getfield(12, "包括可联络时间", "联络电话", phone, STRLEN);
 		getfield(14, "校友会或毕业学校", "校 友 会", assoc, STRLEN);
-		/* only for 9#        getfield( 14, "介绍人ID和真实姓名",    "介绍人(外来的ID须填:ID/姓名)",   assoc, STRLEN );
-		 */
+		/* only for 9#        getfield( 14, "介绍人ID和真实姓名",    "介绍人(外来的ID须填:ID/姓名)",   assoc, STRLEN ); */
 		mesg = "以上资料是否正确, 按 Q 放弃注册 (Y/N/Quit)? [N]: ";
 		getdata(t_lines - 1, 0, mesg, ans, 3, DOECHO, YEA);
 		if (ans[0] == 'Q' || ans[0] == 'q')
@@ -767,8 +766,7 @@ x_fillform()
 		getfield(10, "包括寝室或门牌号码", "目前住址或通讯地址", addr, STRLEN);
 		getfield(12, "包括可联络时间", "联络电话", phone, STRLEN);
 		getfield(14, "校友会或毕业学校", "校 友 会", assoc, STRLEN);
-		/* only for 9#        getfield( 14, "介绍人ID和真实姓名",    "介绍人(外来的ID须填:ID/姓名)",   assoc, STRLEN );
-		 */
+		/* only for 9#        getfield( 14, "介绍人ID和真实姓名",    "介绍人(外来的ID须填:ID/姓名)",   assoc, STRLEN ); */
 		mesg = "以上资料是否正确, 按 Q 放弃注册 (Y/N/Quit)? [N]: ";
 		getdata(t_lines - 1, 0, mesg, ans, 3, DOECHO, YEA);
 		if (ans[0] == 'Q' || ans[0] == 'q')
