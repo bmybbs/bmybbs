0d3d9d7ddd63a784a407035e76ef4103d2fb5c8f
IronBlood <aplshenyang@gmail.com>
2021-01-20 14:24:00 +0800

    format code

--- local_utl/bm/bm.c
+++ local_utl/bm/bm.c
@@ -8,13 +8,11 @@ int main(int argc, char *argv[])
 	int size1 = sizeof (rec);
 	fd1 = fopen(MY_BBS_HOME "/.BOARDS", "r");
 	while (fread(&rec, size1, 1, fd1) == 1) {
-		if (!(rec.level & PERM_POSTMASK) && !(rec.level & PERM_NOZAP)
-		    && rec.level != 0)
+		if (!(rec.level & PERM_POSTMASK) && !(rec.level & PERM_NOZAP) && rec.level != 0)
 			continue;
 		if (rec.clubnum != 0 && (!(rec.flag & CLUBTYPE_FLAG)))
 			continue;
-		printf("%-18s%-24s %s\n", rec.filename,
-		       rec.title, bm2str(bmbuf, &rec));
+		printf("%-18s%-24s %s\n", rec.filename, rec.title, bm2str(bmbuf, &rec));
 	}
 	fclose(fd1);
 	return 0;
