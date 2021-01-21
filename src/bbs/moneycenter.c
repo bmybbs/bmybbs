9befe4b81c3b325191cfe27d03a510806dd12a40
IronBlood <aplshenyang@gmail.com>
2021-01-21 08:12:59 +0800

    补上头文件（coverity-365251 相关）

--- src/bbs/moneycenter.c
+++ src/bbs/moneycenter.c
@@ -18,6 +18,7 @@
 #include "boards.h"
 #include "edit.h"
 #include "bbs-internal.h"
+#include "bcache.h"
 
 #define MC_BOARD        "millionaires"
 #define DIR_MC          MY_BBS_HOME "/etc/moneyCenter/"
