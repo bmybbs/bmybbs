70d7823471282ff78722baafa8afddd9faba9b18
IronBlood <aplshenyang@gmail.com>
2021-01-26 19:14:20 +0800

    format code

--- nju09/www/bbsfwd.c
+++ nju09/www/bbsfwd.c
@@ -1,12 +1,14 @@
 #include "bbslib.h"
 
+static int do_fwd(struct fileheader *x, char *board, char *target);
+
 int
 bbsfwd_main()
 {
 	struct fileheader *x = NULL;
 	char board[80], file[80], target[80], dir[80];
 	int num;
-	struct mmapfile mf = { ptr:NULL };
+	struct mmapfile mf = { .ptr = NULL };
 	html_header(1);
 	check_msg();
 	ytht_strsncpy(board, getparm("B"), 32);
@@ -36,8 +38,7 @@ bbsfwd_main()
 	MMAP_END mmapfile(NULL, &mf);
 	if (x == 0)
 		http_fatal("错误的文件名");
-	printf("<center>%s -- 转寄/推荐给好友 [使用者: %s]<hr>\n", BBSNAME,
-	       currentuser.userid);
+	printf("<center>%s -- 转寄/推荐给好友 [使用者: %s]<hr>\n", BBSNAME, currentuser.userid);
 	if (target[0]) {
 		if (!strstr(target, "@")) {
 			if (!getuser(target))
@@ -53,24 +54,19 @@ bbsfwd_main()
 	printf("<form action=bbsfwd method=post>\n");
 	printf("<input type=hidden name=board value=%s>", board);
 	printf("<input type=hidden name=file value=%s>", file);
-	printf
-	    ("把文章转寄给 <input name=target size=30 maxlength=30 value=%s> (请输入对方的id或email地址). <br>\n",
-	     currentuser.email);
+	printf("把文章转寄给 <input name=target size=30 maxlength=30 value=%s> (请输入对方的id或email地址). <br>\n", currentuser.email);
 	printf("<input type=submit value=确定转寄></form>");
 	return 0;
 }
 
-int
-do_fwd(struct fileheader *x, char *board, char *target)
-{
+static int do_fwd(struct fileheader *x, char *board, char *target) {
 	char title[512], path[200];
 	sprintf(path, "boards/%s/%s", board, fh2fname(x));
 	if (!file_exist(path))
 		http_fatal("文件内容已丢失, 无法转寄");
 	sprintf(title, "[转寄] %s", x->title);
 	title[60] = 0;
-	post_mail(target, title, path, currentuser.userid, currentuser.username,
-		  fromhost, -1, 0);
+	post_mail(target, title, path, currentuser.userid, currentuser.username, fromhost, -1, 0);
 	printf("文章已转寄给'%s'<br>\n", nohtml(target));
 	printf("[<a href='javascript:history.go(-2)'>返回</a>]");
 	return 0;
