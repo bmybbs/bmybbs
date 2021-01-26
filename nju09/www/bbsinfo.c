427eee8ef2c3c570c927f466d31155bb74445595
IronBlood <aplshenyang@gmail.com>
2021-01-26 17:36:11 +0800

    format code

--- nju09/www/bbsinfo.c
+++ nju09/www/bbsinfo.c
@@ -1,5 +1,7 @@
 #include "bbslib.h"
 
+static int check_info();
+
 int
 bbsinfo_main()
 {	//modify by mintbaggio 20040829 for new www
@@ -18,53 +20,36 @@ bbsinfo_main()
 	}
 	printf("<form action=bbsinfo?type=1 method=post>");
 	printf("您的帐号: %s<br>\n", currentuser.userid);
-	printf
-	    ("您的昵称: <input type=text name=nick value='%s' size=24 maxlength=30><br>\n",
-	     currentuser.username);
+	printf("您的昵称: <input type=text name=nick value='%s' size=24 maxlength=30><br>\n", currentuser.username);
 	printf("发表大作: %d 篇<br>\n", currentuser.numposts);
 //      printf("信件数量: %d 封<br>\n", currentuser.nummails);
 	printf("上站次数: %d 次<br>\n", currentuser.numlogins);
 	printf("上站时间: %ld 分钟<br>\n", currentuser.stay / 60);
-	printf
-	    ("真实姓名: <input type=text name=realname value='%s' size=16 maxlength=16><br>\n",
-	     currentuser.realname);
-	printf
-	    ("居住地址: <input type=text name=address value='%s' size=40 maxlength=40><br>\n",
-	     currentuser.address);
+	printf("真实姓名: <input type=text name=realname value='%s' size=16 maxlength=16><br>\n", currentuser.realname);
+	printf("居住地址: <input type=text name=address value='%s' size=40 maxlength=40><br>\n", currentuser.address);
 	printf("帐号建立: %s<br>", ytht_ctime(currentuser.firstlogin));
 	printf("最近光临: %s<br>", ytht_ctime(currentuser.lastlogin));
 	printf("来源地址: %s<br>", currentuser.lasthost);
-#ifndef POP_CHECK	
-	printf
-	    ("电子邮件: <input type=text name=email value='%s' size=32 maxlength=32><br>\n",
-	     currentuser.email);
+#ifndef POP_CHECK
+	printf("电子邮件: <input type=text name=email value='%s' size=32 maxlength=32><br>\n", currentuser.email);
 #endif
 
 #if 0
-	printf
-	    ("出生日期: <input type=text name=year value=%d size=4 maxlength=4>年",
-	     currentuser.birthyear + 1900);
-	printf("<input type=text name=month value=%d size=2 maxlength=2>月",
-	       currentuser.birthmonth);
-	printf("<input type=text name=day value=%d size=2 maxlength=2>日<br>\n",
-	       currentuser.birthday);
+	printf("出生日期: <input type=text name=year value=%d size=4 maxlength=4>年", currentuser.birthyear + 1900);
+	printf("<input type=text name=month value=%d size=2 maxlength=2>月", currentuser.birthmonth);
+	printf("<input type=text name=day value=%d size=2 maxlength=2>日<br>\n", currentuser.birthday);
 	printf("用户性别: ");
-	printf("男<input type=radio value=M name=gender %s>",
-	       currentuser.gender == 'M' ? "checked" : "");
-	printf("女<input type=radio value=F name=gender %s><br>",
-	       currentuser.gender == 'F' ? "checked" : "");
+	printf("男<input type=radio value=M name=gender %s>", currentuser.gender == 'M' ? "checked" : "");
+	printf("女<input type=radio value=F name=gender %s><br>", currentuser.gender == 'F' ? "checked" : "");
 #endif
-	printf
-	    ("<input type=submit value=确定> <input type=reset value=复原>\n");
+	printf("<input type=submit value=确定> <input type=reset value=复原>\n");
 	printf("</form>");
 	printf("<hr>");
 	printf("</body></html>");
 	return 0;
 }
 
-int
-check_info()
-{
+static int check_info() {
 	size_t m;
 	char buf[256];
 	ytht_strsncpy(buf, getparm("nick"), 30);
