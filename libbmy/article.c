d00ad411e0dadeb51814a11406939561a6dfa186
IronBlood <aplshenyang@gmail.com>
2021-02-09 20:24:36 +0800

    移除 bmy_article_list_subscription_by_time 接口

--- libbmy/article.c
+++ libbmy/article.c
@@ -235,14 +235,6 @@ struct bmy_articles *bmy_article_list_subscription(const char *userid, size_t li
 	return bmy_article_list_internal(sqlbuf);
 }
 
-struct bmy_articles *bmy_article_list_subscription_by_time(const char *userid, size_t limit, time_t t) {
-	char sqlbuf[160];
-
-	snprintf(sqlbuf, sizeof(sqlbuf), "SELECT `boardname_en`, `boardname_zh`, `timestamp`, `title`, `author`, `comments`, `accessed` from v_feed_%s WHERE `timestamp` <= %ld LIMIT %zu", userid, t, limit);
-
-	return bmy_article_list_internal(sqlbuf);
-}
-
 struct bmy_articles *bmy_article_list_selected_boards(const int boardnum_array[], size_t num, size_t limit, time_t t) {
 	char *sqlbuf = NULL, *s = NULL;
 	size_t size = 0;
