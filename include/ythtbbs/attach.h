ffb99a6cf526a7fbe098acfdf77d0d2a2d6e2424
IronBlood <aplshenyang@gmail.com>
2021-02-11 17:11:12 +0800

    format code

--- include/ythtbbs/attach.h
+++ include/ythtbbs/attach.h
@@ -1,10 +1,10 @@
-#ifndef __ATTACH_H
-#define __ATTACH_H
+#ifndef YTHTBBS_ATTACH_H
+#define YTHTBBS_ATTACH_H
 #include <stdio.h>
 void filter_attach(char *filename);
 int insertattachments(char *filename, char *content, char *userid);
 int getattach(FILE * fp, char *currline, char *attachfile, char *nowfile, int base64, int len, int fake);
 int insertattachments_byfile(char *filename, char *tmpfile, char *userid);
-int decode_attach(char *filename, char *path); 
+int decode_attach(char *filename, char *path);
 int copyfile_attach(char *source, char *target);
 #endif
