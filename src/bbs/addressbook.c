f1da44494c64966eaf402a2e31f76809a066a72a
IronBlood <aplshenyang@gmail.com>
2021-02-27 18:21:49 +0800

    format code

--- src/bbs/addressbook.c
+++ src/bbs/addressbook.c
@@ -123,11 +123,9 @@ showaddressitem(char *item, int i, int mode)
 	if (item[0] == 0) {
 		prints("%-10s 尚未设定\n", addressitems[i]);
 	} else if (mode == -1) {
-		prints("%-10s %-10s %s\n", addressitems[i],
-		       openlevel[item[0] - '0'], item + 1);
+		prints("%-10s %-10s %s\n", addressitems[i], openlevel[item[0] - '0'], item + 1);
 	} else {
-		prints("%-10s %s\n", addressitems[i],
-		       (mode >= item[0] - '0') ? (item + 1) : "保密");
+		prints("%-10s %s\n", addressitems[i], (mode >= item[0] - '0') ? (item + 1) : "保密");
 	}
 }
 
