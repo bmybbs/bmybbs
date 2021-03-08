a921e504fb96e57a142783ef7d45f99bff7f4e0b
IronBlood <aplshenyang@gmail.com>
2021-03-08 23:12:57 +0800

    重构: 按模块加载 bootstrap js

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import 'bootstrap'
+import '@/plugins/bootstrap.js'
 import '@/plugins/bootstrap.scss'
 import { FontAwesomeIcon } from "./plugins/font-awesome.js"
 import Toaster from "@meforma/vue-toaster"
---
+++ web/src/plugins/bootstrap.js
@@ -0,0 +1,8 @@
+import "bootstrap/js/dist/button";
+import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/popover";
+import "bootstrap/js/dist/tab";
+import "bootstrap/js/dist/tooltip";
+
