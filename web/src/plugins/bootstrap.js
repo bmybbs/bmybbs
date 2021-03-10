3d56d2fd003f206f5e92d50ee27b025f53751cad
IronBlood <aplshenyang@gmail.com>
2021-03-10 09:05:20 +0800

    从全局 js 中移除 bootstrap/popover

--- web/src/plugins/bootstrap.js
+++ web/src/plugins/bootstrap.js
@@ -2,7 +2,6 @@ import "bootstrap/js/dist/button";
 import "bootstrap/js/dist/collapse";
 import "bootstrap/js/dist/dropdown";
 import "bootstrap/js/dist/modal";
-import "bootstrap/js/dist/popover";
 import "bootstrap/js/dist/tab";
 import "bootstrap/js/dist/tooltip";
 
