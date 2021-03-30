c0e6fc42e3f97c8caddd11538c141def157415ae
IronBlood <aplshenyang@gmail.com>
2021-03-30 20:19:22 +0800

    重构: 使用 bootstrap 重构布局

--- web/src/components/CardUserInfo.vue
+++ web/src/components/CardUserInfo.vue
@@ -1,19 +1,32 @@
 <template>
 	<div v-if="info">
-		<div class="title">
-			<span class="userid">{{ info.userid }}</span>
-			<span>{{ info.exp_level }}</span>
-			<span>{{ info.job }}</span>
+		<div class="d-flex mb-1">
+			<span class="fw-bold me-1">{{ info.userid }}</span>
+			<span class="badge rounded-pill bg-secondary">{{ info.job }}</span>
 		</div>
-		<div class="figure">
-			<div class="item">
-				<div class="number">{{ info.login_counts }}</div>
-				<div class="text">次上线</div>
+
+		<div class="mb-1 text-secondary">{{ info.nickname }}</div>
+
+		<div class="d-flex justify-content-between mb-1">
+			<div class="item d-flex justify-content-between me-1">
+				<div>登录</div>
+				<div class="badge rounded-pill bg-secondary">{{ info.login_counts }}</div>
 			</div>
 
-			<div class="item">
-				<div class="number">{{ info.post_counts }}</div>
-				<div class="text">篇文章</div>
+			<div class="item d-flex justify-content-between">
+				<div>发文</div>
+				<div class="badge rounded-pill bg-secondary">{{ info.post_counts }}</div>
+			</div>
+		</div>
+		<div class="d-flex justify-content-between">
+			<div class="item d-flex justify-content-between me-1">
+				<div>经验</div>
+				<div class="badge rounded-pill bg-secondary">{{ info.exp_level }}</div>
+			</div>
+
+			<div class="item d-flex justify-content-between">
+				<div>表现</div>
+				<div class="badge rounded-pill bg-secondary">{{ info.perf_level }}</div>
 			</div>
 		</div>
 	</div>
@@ -40,25 +53,6 @@ export default {
 </script>
 
 <style scoped>
-.userid {
-	font-weight: 700;
-}
-
-.figure {
-	display: flex;
-	justify-content: space-between;
-}
-
-.figure .item {
-	display: flex;
-}
-
-.figure .item:not(:first-child) {
-	margin-left: 10px;
-}
-
-.figure .number {
-	font-weight: 700;
-}
+.item { width: 10rem; }
 </style>
 
