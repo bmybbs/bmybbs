110383dc852a06d0728329a0b9b84fbc5fe46709
IronBlood <aplshenyang@gmail.com>
2021-04-09 00:34:52 +0800

    处理分区主题列表的顶部下拉刷新以及页底追加

--- minibmy/pages/section/section.js
+++ minibmy/pages/section/section.js
@@ -1,12 +1,32 @@
 import { BMYClient } from "../../utils/BMYClient.js"
 import { BOARD_SORT_MODE, BMYSECSTRS } from "../../utils/BMYConstants.js"
 
+const update_article_callback = (response, article_arr, set, page, that) => {
+	if (Array.isArray(response.articles)) {
+		response.articles.forEach((x) => {
+			x.id = x.boardname_en + x.tid;
+			if (!set.has(x.id)) {
+				set.add(x.id);
+				article_arr.push(x);
+			}
+		});
+
+		that.setData({
+			articles: article_arr,
+			feedSet: set,
+			page: page + 1
+		});
+	}
+};
+
 Page({
 	data: {
 		articles: [],
 		boards: [],
 		sec_en: "",
 		sec_zh: "",
+		feedSet: new Set(),
+		page: 1,
 		activeTab: 0,
 		tabs: [
 			{ title: "话题", },
@@ -14,15 +34,8 @@ Page({
 		]
 	},
 	onLoad: function (options) {
-		BMYClient.get_article_list_by_section(options.id).then(response => {
-			if (Array.isArray(response.articles)) {
-				response.articles.forEach((x) => {
-					x.id = x.boardname_en + x.tid;
-				});
-				this.setData({
-					articles: response.articles
-				});
-			}
+		BMYClient.get_article_list_by_section(options.id, 1).then(response => {
+			update_article_callback(response, [], new Set(), 1, this);
 		});
 		BMYClient.get_boards_by_section(options.id, BOARD_SORT_MODE.BY_ALPHABET).then(response => {
 			if (response.errcode == 0) {
@@ -48,6 +61,20 @@ Page({
 	onChange(e) {
 		this.setData({
 			activeTab: e.detail.index
+		});
+	},
+	onFeedEnd() {
+		BMYClient.get_article_list_by_section(this.data.sec_en, this.data.page).then(response => {
+			update_article_callback(response, this.data.articles, this.data.feedSet, this.data.page, this);
+		});
+	},
+	onFeedRefresh() {
+		BMYClient.get_article_list_by_section(this.data.sec_en, 1).then(response => {
+			this.setData({
+				feedRefreshTriggered: false
+			});
+
+			update_article_callback(response, [], new Set(), 1, this);
 		});
 	},
 })
