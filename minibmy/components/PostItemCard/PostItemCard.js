30b46af4f47c8b7b7073153da43bddfb74293996
IronBlood <aplshenyang@gmail.com>
2021-04-09 12:18:59 +0800

    重构: PostItemCard 调整布局，增加话题跳转

--- minibmy/components/PostItemCard/PostItemCard.js
+++ minibmy/components/PostItemCard/PostItemCard.js
@@ -1,3 +1,5 @@
+import { getReadableTime } from "../../utils/Time.js"
+
 Component({
 	properties: {
 		postItem: {
@@ -6,7 +8,17 @@ Component({
 	},
 	data: {
 	},
+	attached() {
+		this.setData({
+			readableTime: getReadableTime(this.data.postItem.tid),
+		});
+	},
 	methods: {
+		gotoThread() {
+			wx.navigateTo({
+				url: `../../pages/thread/thread?boardname_en=${this.data.postItem.board}&tid=${this.data.postItem.tid}`,
+			});
+		},
 	}
 })
 
