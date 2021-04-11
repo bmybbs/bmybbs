08a032fc075ee7c51ee8ef023bf090befc2cb8a0
IronBlood <aplshenyang@gmail.com>
2021-04-11 23:44:11 +0800

    新增底部 overlay 方式的编辑框

---
+++ minibmy/components/EditorOverlay/EditorOverlay.js
@@ -0,0 +1,54 @@
+Component({
+	properties: {
+		callback: { type: Function },
+		isReply:  { type: Boolean, value: false },
+		show:     { type: Boolean },
+		title:    { type: String },
+	},
+	data: {
+		doClose: false,
+		body: "",
+	},
+	attached() {
+	},
+	methods: {
+		doNothing() {
+			// 本方法用于 input / textarea bindinput
+			// 启用双向绑定 model:value 方式后不清楚什么原因，在 input / textarea 输入文本
+			// 后会在控制台抛出
+			// Do not have  handler in component: components/EditorOverlay/EditorOverlay.
+			// Please make sure that  handler has been defined in
+			// components/EditorOverlay/EditorOverlay.
+			// 的警告信息，腾讯的文档实在是 ...
+		},
+		preventClose() {
+			if (!this.data.doClose) {
+				this.setData({ show: true });
+			}
+		},
+		closeOverlay() {
+			this.setData({ doClose: true, show: false });
+		},
+		cleanInput() {
+			this.setData({ title: "", body: "" });
+		},
+		post() {
+			if (this.data.callback != null && typeof this.data.callback === "function") {
+				const obj = {
+					isReply: this.data.isReply,
+					body: this.data.body,
+				};
+
+				if (!this.data.isReply) {
+					obj.title = this.data.title;
+				}
+
+				this.data.callback(obj, () => {
+					this.cleanInput();
+					this.closeOverlay();
+				});
+			}
+		},
+	},
+});
+
