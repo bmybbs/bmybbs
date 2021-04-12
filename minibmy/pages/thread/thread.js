8f5b51e477dba470f8c8677c4763602c1cc31884
IronBlood <aplshenyang@gmail.com>
2021-04-12 19:07:13 +0800

    同主题阅读使用 editor-overlay 处理回帖

--- minibmy/pages/thread/thread.js
+++ minibmy/pages/thread/thread.js
@@ -1,16 +1,61 @@
 import { BMYClient } from "../../utils/BMYClient.js"
 
+let articleInstance;
+
 Page({
 	data: {
+		editorShow: false,
+		editorTitle: "",
+		board: "",
+		tid: "",
+		articlelist: [],
+		set: new Set(),
 	},
 	onLoad: function(options) {
-		BMYClient.get_thread_list(options.boardname_en, options.tid).then(response => {
+		this.setData({ board: options.boardname_en, tid: options.tid });
+		this.loadThread(options.boardname_en, options.tid);
+	},
+	onOpenEditor(e) {
+		articleInstance = e.detail && e.detail.that;
+		this.setData({
+			editorShow: true,
+			editorTitle: e.detail.title,
+		});
+	},
+	onPost(e) {
+		if (articleInstance) {
+			const callback = e.detail.onSuccess;
+
+			e.detail.onSuccess = () => {
+				if (callback !== null && typeof callback === "function") {
+					callback();
+				}
+				// 追加自身的回调
+				this.loadThread(this.data.board, this.data.tid, true);
+			}
+
+			articleInstance.doReply(e.detail);
+		}
+	},
+	loadThread(board, tid, append = false) {
+		const arr = append ? this.data.articlelist : [],
+			set   = append ? this.data.set         : new Set();
+
+		BMYClient.get_thread_list(board, tid).then(response => {
 			if (response.errcode == 0 && Array.isArray(response.articlelist) && response.articlelist.length > 0) {
+				response.articlelist.forEach(x => {
+					if (!set.has(x.aid)) {
+						arr.push(x);
+						set.add(x.aid);
+					}
+				});
+
 				this.setData({
-					articlelist: response.articlelist
+					articlelist: arr,
+					set: set,
 				});
 			}
 		});
-	},
+	}
 });
 
