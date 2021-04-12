e6c155ca854738de05c1523ac3d6f984093a8d77
IronBlood <aplshenyang@gmail.com>
2021-04-12 17:10:50 +0800

    新增 generateContent 函数（和 web/ 一致）

--- minibmy/utils/ArticleUtils.js
+++ minibmy/utils/ArticleUtils.js
@@ -18,3 +18,31 @@ export const minibmyFormatArticle = content => {
 	return new XMLSerializer().serializeToString(doc);
 }
 
+/**
+ * 生成回复全文
+ * @param text   正文
+ * @param author 原文作者
+ * @param origin 原文
+ * @return 全文
+ */
+export const generateContent = (text, author, origin) => {
+	const arr = origin.split("\n"),
+		result = [ text, `【 在 ${author} 的大作中提到: 】` ];
+
+	for (let i = 0, j = 0, l = arr.length; i < l && j < 5; i++) {
+		if (arr[i] == "")
+			continue;
+
+		result.push(`: ${arr[i]}`);
+		j++;
+	}
+
+	if (result.length == 2) {
+		result.push(": ");
+	} else {
+		result.push(": ...................");
+	}
+
+	return result.join("\n");
+}
+
