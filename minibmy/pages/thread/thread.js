30634a38f9542bcbc23f26c9967952b010acfec3
IronBlood <aplshenyang@gmail.com>
2021-04-12 13:30:53 +0800

    同主题阅读移除顶部信息

--- minibmy/pages/thread/thread.js
+++ minibmy/pages/thread/thread.js
@@ -7,8 +7,6 @@ Page({
 		BMYClient.get_thread_list(options.boardname_en, options.tid).then(response => {
 			if (response.errcode == 0 && Array.isArray(response.articlelist) && response.articlelist.length > 0) {
 				this.setData({
-					thread_title: response.articlelist[0].title,
-					boardname_en: response.articlelist[0].board,
 					articlelist: response.articlelist
 				});
 			}
