fbd1a7a2b3ddb7fb115e57a41dc77955aad15d9e
IronBlood <aplshenyang@gmail.com>
2021-05-28 15:53:42 +0800

    format code

--- nju09/www/bbsdenyadd.c
+++ nju09/www/bbsdenyadd.c
@@ -7,7 +7,7 @@ int
 bbsdenyadd_main()
 {
 	int i;
-	char buf[256], exp[80], board[80], *userid;
+	char buf[256], expbuf[80], board[80], *userid;
 	int dt;
 	struct userec *x;
 	struct boardmem *x1;
@@ -17,7 +17,7 @@ bbsdenyadd_main()
 		http_fatal("您尚未登录, 请先登录");
 	changemode(READING);
 	ytht_strsncpy(board, getparm("board"), 30);
-	ytht_strsncpy(exp, getparm("exp"), 30);
+	ytht_strsncpy(expbuf, getparm("exp"), 30);
 	dt = atoi(getparm("dt"));
 	if (!(x1 = getboard(board)))
 		http_fatal("错误的讨论区");
@@ -36,7 +36,7 @@ bbsdenyadd_main()
 		http_fatal("封禁时间大于20天,超过了权限,若需要,请联系站长");
 	if (dt < 1 || dt > 99)
 		http_fatal("请输入被封天数(1-99)");
-	if (exp[0] == 0)
+	if (expbuf[0] == 0)
 		http_fatal("请输入封人原因");
 	for (i = 0; i < denynum; i++)
 		if (!strcasecmp(denyuser[i].id, userid))
@@ -44,14 +44,14 @@ bbsdenyadd_main()
 	if (denynum > 40)
 		http_fatal("太多人被封了");
 	ytht_strsncpy(denyuser[denynum].id, userid, 13);
-	ytht_strsncpy(denyuser[denynum].exp, exp, 30);
+	ytht_strsncpy(denyuser[denynum].exp, expbuf, 30);
 	denyuser[denynum].free_time = now_t + dt * 86400;
 	denynum++;
 	savedenyuser(board);
 	printf("封禁 %s 成功<br>\n", userid);
 	snprintf(buf, 256, "%s deny %s %s", currentuser.userid, board, userid);
 	newtrace(buf);
-	inform(board, userid, exp, dt);
+	inform(board, userid, expbuf, dt);
 	printf("[<a href=bbsdenyall?board=%s>返回被封帐号名单</a>]", board);
 	http_quit();
 	return 0;
@@ -61,11 +61,8 @@ static int
 show_form(char *board)
 {
 	printf("<center>%s -- 版务管理 [讨论区: %s]<hr>\n", BBSNAME, board);
-	printf
-	    ("<form action=bbsdenyadd><input type=hidden name=board value='%s'>",
-	     board);
-	printf
-	    ("封禁使用者<input name=userid size=12> 本版POST权 <input name=dt size=2> 天, 原因<input name=exp size=20>\n");
+	printf("<form action=bbsdenyadd><input type=hidden name=board value='%s'>", board);
+	printf("封禁使用者<input name=userid size=12> 本版POST权 <input name=dt size=2> 天, 原因<input name=exp size=20>\n");
 	printf("<input type=submit value=确认></form>");
 	return 0;
 }
@@ -84,18 +81,18 @@ inform(char *board, char *user, char *exp, int dt)
 	fp = fopen(path, "w");
 	fprintf(fp, "【此篇文章是由自动发信系统所张贴】\n\n");
 	snprintf(buf, sizeof (buf),
-		 "封人原因: %s\n"
-		 "被封天数: %d\n"
-		 "解封日期: %d月%d日\n"
-		 "如有异议，可向版主提出，或到 committee 版投诉\n",
-		 exp, dt, tmtime->tm_mon + 1, tmtime->tm_mday);
+		"封人原因: %s\n"
+		"被封天数: %d\n"
+		"解封日期: %d月%d日\n"
+		"如有异议，可向版主提出，或到 committee 版投诉\n",
+		exp, dt, tmtime->tm_mon + 1, tmtime->tm_mday);
 	fputs(buf, fp);
 	fclose(fp);
 	securityreport(title, buf);
 	post_article(board, title, path, "XJTU-XANET", "自动发信系统",
-		     "自动发信系统", -1, 4, 0, "XJTU-XANET", -1);//8 bit 0->4 bjgyt
+			"自动发信系统", -1, 4, 0, "XJTU-XANET", -1);//8 bit 0->4 bjgyt
 	post_mail(user, title, path, currentuser.userid, currentuser.username,
-		  fromhost, -1, 0);
+			fromhost, -1, 0);
 	unlink(path);
 	printf("系统已经发信通知了%s.<br>\n", user);
 	return 0;
