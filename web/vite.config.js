0b69770216f2c6a0025af9877cf5c9795adb0692
IronBlood <aplshenyang@gmail.com>
2023-02-19 00:58:54 +0800

    chore: :wrench: 增加 vite proxy 配置，用于开发阶段和 api / nju09 通信

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,19 @@ import vue from "@vitejs/plugin-vue";
 
 import path from "path";
 
+import fs from "fs";
+const proxy_config_file = path.resolve(__dirname, "./proxy.config.txt");
+
+if (!fs.existsSync(proxy_config_file)) {
+	console.error("ERROR: Proxy config file doesn't exist!");
+	console.error("       To create this config file, here's an example");
+	console.error("       echo 'http://ip:port' > /path/to/web/.proxy.txt");
+	console.error("       where bmyapi listens on ip:port");
+	process.exit();
+}
+
+const PROXY_CONFIG = fs.readFileSync(proxy_config_file, "utf8").trim();
+
 export default defineConfig({
 	plugins: [
 		vue(),
@@ -10,6 +23,20 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		host: true,
+		proxy: {
+			"^/BMY" : {
+				target: PROXY_CONFIG,
+			},
+			"^/bmyMainPic": {
+				target: PROXY_CONFIG,
+			},
+			"^/api": {
+				target: PROXY_CONFIG,
+			}
 		},
 	},
 });
