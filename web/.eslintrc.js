bd6374271f5f2e3d045e09e6f26576e997fbae98
IronBlood <aplshenyang@gmail.com>
2023-02-19 00:25:21 +0800

    build: :building_construction: 使用 vite 取代 vue-cli / webpack
    close #386
    close #391

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,15 +1,12 @@
 module.exports = {
 	root: true,
 	env: {
-		node: true
+		es2021: true,
 	},
 	extends: [
 		"plugin:vue/vue3-essential",
 		"eslint:recommended"
 	],
-	parserOptions: {
-		parser: "babel-eslint"
-	},
 	rules: {
 	}
 };
