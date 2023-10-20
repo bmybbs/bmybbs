50c89ce0c46e93a22d64fa4b6f2f6bd587d471b4
IronBlood <aplshenyang@gmail.com>
2023-10-20 15:51:45 +0800

    fix(snprintf): use snprintf to replace sprintf

--- libythtbbs/cache/utmp.c
+++ libythtbbs/cache/utmp.c
@@ -103,7 +103,7 @@ int ythtbbs_cache_utmp_insert(struct user_info *ptr_user_info) {
 			if (ptr_utmp_entry->pid == 1) {
 				// 对于来自 nju09 和 api 的会话清理
 				if (ptr_utmp_entry->active && (local_now - ptr_utmp_entry->lasttime > MAX_IDEL_TIME || local_now - ptr_utmp_entry->wwwinfo.login_start_time > MAX_SESS_TIME || ptr_utmp_entry->wwwinfo.iskicked)) {
-					sprintf(local_buf, "%s drop www/api", ptr_utmp_entry->userid);
+					snprintf(local_buf, sizeof(local_buf), "%s drop www/api", ptr_utmp_entry->userid);
 					newtrace(local_buf);
 					ythtbbs_cache_UserTable_remove_utmp_idx(ptr_utmp_entry->uid, n);
 					ythtbbs_session_del(ptr_utmp_entry->sessionid);
