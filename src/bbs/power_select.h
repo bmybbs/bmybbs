d55773b1719feb676291ad61ae76d020dfe13e24
IronBlood <aplshenyang@gmail.com>
2026-04-01 11:28:09 +0800

    add header

--- src/bbs/power_select.h
+++ src/bbs/power_select.h
@@ -1,5 +1,7 @@
 #ifndef BMYBBS_POWER_SELECT_H
 #define BMYBBS_POWER_SELECT_H
+#include "ythtbbs/article.h"
+
 int full_search_action(char *whattosearch);
 int power_action(char *filename, unsigned int id1, int id2, char *select, int action);
 const char *strstr2(const char *s, const char *s2);
