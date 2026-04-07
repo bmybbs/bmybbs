eda08c1ddfb96edaa50954e6d9727ee12aae8437
IronBlood <aplshenyang@gmail.com>
2026-04-07 20:57:37 +0800

    fix: 增大缓冲区

--- src/bbs/goodbye.c
+++ src/bbs/goodbye.c
@@ -8,7 +8,7 @@
     Firebird Bulletin Board System
     Copyright (C) 1996, Hsien-Tsung Chang, Smallpig.bbs@bbs.cs.ccu.edu.tw
                         Peng Piaw Foong, ppfoong@csie.ncu.edu.tw
-    
+
     Copyright (C) 1999, KCN,Zhou Lin, kcn@cic.tsinghua.edu.cn
 
     This program is free software; you can redistribute it and/or modify
@@ -81,7 +81,7 @@ void showstuff(char *buf) {
 	extern time_t login_start_time;
 	int frg, i, matchfrg, strlength, cnt, tmpnum;
 	static char numlogins[10], numposts[10], rgtday[35], lasttime[35],
-	    thistime[35], lastlogout[35], stay[10], alltime[20], ccperf[20],
+	    thistime[35], lastlogout[35], stay[24], alltime[32], ccperf[20],
 	    perf[10], exp[10], ccexp[20];
 	char buf2[STRLEN], *ptr, *ptr2;
 	time_t now;
@@ -123,15 +123,15 @@ void showstuff(char *buf) {
 	sprintf(perf, "%d", tmpnum);
 	strcpy(ccperf, cperf(tmpnum));
 	// %ld小时%ldx分钟
-	sprintf(alltime, "%ld\xD0\xA1\xCA\xB1%ld\xB7\xD6\xD6\xD3", (long int) (currentuser.stay / 3600),
+	snprintf(alltime, sizeof alltime, "%ld\xD0\xA1\xCA\xB1%ld\xB7\xD6\xD6\xD3", (long int) (currentuser.stay / 3600),
 		(long int) ((currentuser.stay / 60) % 60));
-	sprintf(rgtday, "%24.24s", ctime(&currentuser.firstlogin));
-	sprintf(lasttime, "%24.24s", ctime(&currentuser.lastlogin));
-	sprintf(lastlogout, "%24.24s", ctime(&currentuser.lastlogout));
-	sprintf(thistime, "%24.24s", ctime(&now));
-	sprintf(stay, "%ld", (long int) ((time(0) - login_start_time) / 60));
-	sprintf(numlogins, "%d", currentuser.numlogins);
-	sprintf(numposts, "%d", currentuser.numposts);
+	snprintf(rgtday, sizeof rgtday, "%24.24s", ctime(&currentuser.firstlogin));
+	snprintf(lasttime, sizeof lasttime, "%24.24s", ctime(&currentuser.lastlogin));
+	snprintf(lastlogout, sizeof lastlogout, "%24.24s", ctime(&currentuser.lastlogout));
+	snprintf(thistime, sizeof thistime, "%24.24s", ctime(&now));
+	snprintf(stay, sizeof stay, "%ld", ((time(0) - login_start_time) / 60));
+	snprintf(numlogins, sizeof numlogins, "%d", currentuser.numlogins);
+	snprintf(numposts, sizeof numposts, "%d", currentuser.numposts);
 
 	frg = 1;
 	ptr2 = buf;
