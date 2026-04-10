661891b271392182a3a329f20ce12438172f93c5
IronBlood <aplshenyang@gmail.com>
2026-04-10 19:04:08 +0800

    chore: 不再编译和安装 setdefaultkey

--- install_binaries_and_libraries.sh
+++ install_binaries_and_libraries.sh
@@ -74,6 +74,5 @@ else
 	#install_binary $BUILD_DIR/local_utl/printSecLastUpdate/printSecLastUpdate printSecLastUpdate
 	install_binary $BUILD_DIR/local_utl/save_brc/save_brc                     save_brc
 	install_binary $BUILD_DIR/local_utl/searchLastMark/searchLastMark         searchLastMark
-	install_binary $BUILD_DIR/local_utl/setdefaultkey/setdefaultkey           setdefaultkey
 	install_binary $BUILD_DIR/local_utl/sortdir/sortdir                       sortdir
 fi
